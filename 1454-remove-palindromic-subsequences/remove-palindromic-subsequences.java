8061c6bf1ed373f77c9241c93201715f0f0a9b70
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2025-08-18 19:48:09 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (56.35%) - LeetSync

---
+++ 1454-remove-palindromic-subsequences/remove-palindromic-subsequences.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int removePalindromeSub(String s) {
+        if(ispalindrome(s))
+        return 1;
+        return 2;
+    }
+ 
+    	public static boolean ispalindrome(String s){
+		int i=0,j=s.length()-1;
+		while(i<j) {
+		  if(s.charAt(i)!=s.charAt(j))
+			return false;
+		i++;
+		j--;
+		}
+		return true;
+	}
+}
