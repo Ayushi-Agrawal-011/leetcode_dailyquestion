7185532cb6fc90b7f5bc345271d4470e68a32305
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2025-08-24 18:51:50 +0530

    Time: 34 ms (5.98%) | Memory: 51.3 MB (12.89%) - LeetSync

---
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int longestCommonSubsequence(String s1, String s2) {
+       int [][]dp=new int[s1.length()][s2.length()];
+       for(int i = 0; i < dp.length; i++) {
+    for(int j = 0; j < dp[0].length; j++) {
+        dp[i][j] = -1;
+    }
+}
+return s(s1,s2,0,0,dp);
+    }
+     public int s(String s1, String s2,int i,int j,int [][]dp) {
+        if(i==s1.length() || j==s2.length())
+        return 0;
+        if(dp[i][j]!=-1)
+        return dp[i][j];
+        int ans=0;
+      if(s1.charAt(i)==s2.charAt(j)) {
+   ans=1+s(s1,s2,i+1,j+1,dp);
+      }
+      else{
+        int f=s(s1,s2,i+1,j,dp);
+          int t=s(s1,s2,i,j+1,dp);
+          ans=Math.max(f,t);
+      }
+      return dp[i][j]=ans;
+    }
+}
