3d06beb66e5d4c24584da204bdd46b6190bc81ac
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2025-09-26 20:46:44 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (11.99%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,9 +1,31 @@
 class Solution {
     public int search(int[] nums, int target) {
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==target)
-            return i;
-        }
-        return -1;
+      
+        return binary_search(nums,target);
     }
+    	public static int binary_search(int [] arr,int item) {
+         
+		    int start=0;
+	       int last=arr.length-1;
+	    
+	       for(int mid;start<=last;) {
+	    	   mid=start+(last-start)/2;
+               if(arr[mid]==item)
+               return mid;
+	    	   if(arr[start]<=arr[mid]){
+                if(item>=arr[start] && item<arr[mid])
+                last=mid-1;
+                else
+                start=mid+1;
+               }
+               else {
+                  if(item>arr[mid] && item<=arr[last])
+                    start=mid+1;
+                else
+                   last=mid-1;
+               }
+	    	  
+	       }
+	       return -1;
+	}
 }
