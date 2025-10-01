8f7e8d9a79d8e2b11f37460ea595157d6177b575
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2025-10-01 15:09:37 +0530

    Time: 3 ms (80.30%) | Memory: 45.6 MB (81.42%) - LeetSync

---
+++ 135-candy/candy.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int candy(int[] ratings) {
+        int n=ratings.length;
+        int [] arr=new int[n];
+        Arrays.fill(arr,1);
+        for(int i=1;i<ratings.length;i++){
+            if(ratings[i]>ratings[i-1]){
+                arr[i]=arr[i-1]+1;
+            }
+            // System.out.println(arr[i]);
+
+        }
+          for(int i=n-2;i>=0;i--){
+            
+            if(ratings[i]>ratings[i+1]){
+                arr[i]=Math.max(arr[i],arr[i+1]+1);
+            }
+
+        }
+int s=0;
+for(int i=0;i<n;i++){
+    s+=arr[i];
+}
+return s;
+    }
+}
