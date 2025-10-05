935dc495a4eba637d00b130db18b0791c29512e5
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2025-10-05 19:40:09 +0530

    Time: 14 ms (55.21%) | Memory: 45.6 MB (67.95%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.java
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.java
@@ -1,27 +1,23 @@
 class Solution {
     public String frequencySort(String s) {
-            TreeMap<Character, Integer> map = new TreeMap<>();
-         TreeMap<Integer, List<Character>> mp = new TreeMap<>(Collections.reverseOrder());
-
-        for (char num : s.toCharArray()) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
-        }
-
-		  for (char num : map.keySet()) {
-            int freq = map.get(num);
-            mp.putIfAbsent(freq, new ArrayList<>());
-            mp.get(freq).add(num);
-        }
-StringBuilder sb=new StringBuilder();
-
-		  for (int freq : mp.keySet()) {
-            for (char num : mp.get(freq)) {
-                for(int i=0;i<freq;i++){
-             sb.append(num);
+            TreeMap<Character,Integer> map=new TreeMap<>();
+            TreeMap<Integer,List<Character>> mp=new TreeMap<>(Collections.reverseOrder());
+            for(char ch:s.toCharArray()){
+                map.put(ch,map.getOrDefault(ch,0)+1);
             }
-            }  
-        }
+            for(char ch:map.keySet()){
+                mp.putIfAbsent(map.get(ch),new ArrayList<>());
+                mp.get(map.get(ch)).add(ch);
+            }
+            StringBuilder sb=new StringBuilder();
+              for(int x:mp.keySet()){
+             for(char p:mp.get(x)){
+                for(int i=0;i<x;i++){
+                    sb.append(p);
+                }
+             }
+            }
+            return sb.toString();
 
-        return sb.toString();
     }
 }
