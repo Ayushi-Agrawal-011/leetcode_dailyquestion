b2bc390199d3a7459567532391ab52e723943a55
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2025-10-06 10:52:41 +0530

    Time: 0 ms (100.00%) | Memory: 40.3 MB (81.95%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,19 +1,21 @@
 class Solution {
     public int fib(int n) {
- int [] dp=new int[n+1];
-Arrays.fill(dp,-1);
- return fibdp(n,dp);
+
+ //return fibdp(n,dp);
+ return fibdpBu(n);
     }
 
-public int fibdp(int  n,int []dp){
-if(n==0 || n==1){
 
-    return n;
+public int fibdpBu(int  n){
+ int [] dp=new int[n+1];
+ if(n==0)
+ return 0;
+dp[0]=0;
+dp[1]=1;
+for(int i=2;i<=n;i++){
+    dp[i]=dp[i-1]+dp[i-2];
 }
-if(dp[n]!=-1)
 return dp[n];
-int a=fibdp(n-1,dp);
-int b=fibdp(n-2,dp);
-return dp[n]=a+b;
+
 }
 }
