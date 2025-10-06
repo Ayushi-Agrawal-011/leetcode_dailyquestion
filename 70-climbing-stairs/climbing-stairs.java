994335834c644ecb1a7989398003fcfe56d65e70
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2025-10-06 11:29:26 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (5.73%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,20 +1,20 @@
 class Solution {
 int c=0;
       public int climbStairs(int n) {
-   int [] dp=new int[n+1];
-Arrays.fill(dp,-1);
- return fibdp(n,dp);
+   return fibdpBu(n+1);
 
 	}
-  public int fibdp(int  n,int []dp){
-if(n==0 || n==1){
+  public int fibdpBu(int  n){
 
-    return 1;
+ if(n==0 || n==1)
+ return n;
+  int [] dp=new int[n+1];
+dp[0]=0;
+dp[1]=1;
+for(int i=2;i<=n;i++){
+    dp[i]=dp[i-1]+dp[i-2];
 }
-if(dp[n]!=-1)
 return dp[n];
-int a=fibdp(n-1,dp);
-int b=fibdp(n-2,dp);
-return dp[n]=a+b;
+
 }
 }
