a74de97705064907cbf466bf3ddc40f76dc55dc7
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2025-10-08 10:57:48 +0530

    Time: 4 ms (21.73%) | Memory: 42.6 MB (5.29%) - LeetSync

---
+++ 763-special-binary-string/special-binary-string.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String makeLargestSpecial(String s) {
+        int count=0;
+        int i=0;
+        List<String> res=new LinkedList<>();
+       for(int j=0;j<s.length();j++){
+        char ch=s.charAt(j);
+        if(ch=='1')
+        count++;
+        else if(ch=='0')
+        count--;
+        if(count==0){
+            res.add("1"+makeLargestSpecial(s.substring(i+1,j))+"0");
+            i=j+1;
+        }
+       }
+       Collections.sort(res,Collections.reverseOrder());
+       return String.join("",res);
+    }
+}
