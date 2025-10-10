cd3ce8926875ea0de5e72cfe697187f05d2d1338
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2025-10-10 15:41:55 +0530

    Time: 27 ms (9.21%) | Memory: 54.7 MB (5.45%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -9,16 +9,13 @@ return Coin_Change(coins,amount,0,dp);
     }
     public static int Coin_Change(int[]coin,int amount,int i,int[][]dp) {
 	if(amount==0)
-		return 1;
-	if(i==coin.length) {
-		return 0;
-	}
-	if(dp[amount][i]!=-1)
-		return dp[amount][i];
-	int inc=0,exc=0;
-	if(amount>=coin[i])
-		inc=Coin_Change(coin,amount-coin[i],i,dp);
-	exc=Coin_Change(coin,amount,i+1,dp);
-	return dp[amount][i]=inc+exc;
-}
+    return 1;
+    else if(amount<0 ||i==coin.length)
+    return 0;
+    if(dp[amount][i]!=-1)
+    return dp[amount][i];
+    int inc=Coin_Change(coin,amount-coin[i],i,dp);
+    int exc=Coin_Change(coin,amount,i+1,dp);
+    return dp[amount][i]= inc+exc;
+    }
 }
