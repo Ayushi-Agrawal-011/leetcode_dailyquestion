4e3b0334327303454d1cd5b4f41c2725118baf4a
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2025-10-11 13:17:29 +0530

    Time: 674 ms (5.03%) | Memory: 44.5 MB (11.89%) - LeetSync

--- 526-beautiful-arrangement/beautiful-arrangement.java
+++ 526-beautiful-arrangement/beautiful-arrangement.java
@@ -1,28 +1,26 @@
 class Solution {
+    int c=0;
     public int countArrangement(int n) {
-        List<Integer>curr=new ArrayList<>();
-        List<List<Integer>> res=new ArrayList<>();
-        int q=1;
-
-        int[]nums=new int[n];
-        for(int i=0;i<n;i++){
-            nums[i]=q;
-            q++;
-        }
-        p(nums,curr,res);
-        return res.size();
+      List<List<Integer>> res=new ArrayList<>();
+       List<Integer> curr=new ArrayList<>();
+       int [] nums=new int[n];
+       for(int i=0;i<n;i++){
+        nums[i]=i+1;
+       }
+       p(nums,res,curr);
+       return res.size();
     }
-    public void p(int []nums,List<Integer>curr,List<List<Integer>> res){
+      public void p(int []nums,List<List<Integer>> res,List<Integer> curr){
         if(curr.size()==nums.length){
             res.add(new ArrayList<>(curr));
             return;
         }
         for(int i=0;i<nums.length;i++){
-         if(!curr.contains(nums[i]) &&((curr.size()+1)%nums[i]==0  || nums[i]%(curr.size()+1)==0)){
-            curr.add(nums[i]);
-            p(nums,curr,res);
-      curr.remove(curr.size()-1);
-         }
+            if(!curr.contains(nums[i]) &&( nums[i]%(curr.size()+1)==0 || (curr.size()+1)% nums[i]==0 )){
+                curr.add(nums[i]);
+                p(nums,res,curr);
+                 curr.remove(curr.size()-1);
+            }
         }
     }
 }
