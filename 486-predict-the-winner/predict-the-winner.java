cf2589327269a828ad5048bb8fa609f0971b6dcc
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2025-10-11 12:53:43 +0530

    Time: 71 ms (15.67%) | Memory: 41.1 MB (63.11%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.java
+++ 486-predict-the-winner/predict-the-winner.java
@@ -1,12 +1,13 @@
 class Solution {
     public boolean predictTheWinner(int[] nums) {
-          return score(nums,0,nums.length-1)>=0;
+       return score(nums,0,nums.length-1)>=0;
     }
-    public int score(int []nums,int i,int j){
-        if(i==j)
-        return nums[j];
-int start=nums[i]-score(nums,i+1,j);
-int end=nums[j]-score(nums,i,j-1);
-return Math.max(start,end);
+    public int score(int [] nums,int i,int j){
+if(i==j)
+return  nums[j];
+        int left=nums[i]-score(nums,i+1,j);
+        int right=nums[j]-score(nums,i,j-1);
+       return  Math.max(left,right);
     }
+  
 }
