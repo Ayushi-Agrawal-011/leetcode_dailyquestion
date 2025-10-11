023223a454980ba3db92f78a7e83940a5704dd47
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2025-10-11 13:52:14 +0530

    Time: 1 ms (97.58%) | Memory: 45.5 MB (90.19%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.java
+++ 605-can-place-flowers/can-place-flowers.java
@@ -1,30 +1,26 @@
 class Solution {
-    public boolean canPlaceFlowers(int[]nums, int r) {
-        int c=0;
-        int n=nums.length;
-    for(int i=0;i<n && c<r;i++){
-      if(nums[i]==1){
-        continue;
-      }
-      else if(nums[i]==0){
-        int p=0,q=0;
-        if(i-1>=0 && nums[i-1]==0){
+    public boolean canPlaceFlowers(int[]nums, int r) { int c=0;
+    for(int i=0;i<nums.length && c<r;i++){
+        if(nums[i]==0){
+            int p=0,q=0;
+           if(i-1>=0 && nums[i-1]==0){
             p=1;
-        }
-        else if(i==0)
-        p=1;
-         if(i+1<nums.length && nums[i+1]==0){
+           } 
+           else if(i==0)
+           p=1;
+            if(i+1<nums.length && nums[i+1]==0){
             q=1;
-         }
-         else if(i==nums.length-1){
-            q=1;
-         }
-         if(p==1 && q==1){
-            nums[i]=1;
+           } 
+           else if(i==nums.length-1)
+           q=1;
+           if(p==1 && q==1){
             c++;
-         }
-      }
-    } 
-    return c==r;   
+
+            nums[i]=1;
+           }
+        }
+    }
+    return c==r;
+       
     }
 }
