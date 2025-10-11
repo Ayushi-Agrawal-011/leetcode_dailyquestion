911047435a3f94d11193298ea4ae141d558140c9
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2025-10-11 13:44:36 +0530

    Time: 32 ms (7.38%) | Memory: 50.1 MB (5.66%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,29 +1,35 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] arr) {
-         List<List<String>> res=new ArrayList<>();
-       HashMap<String,List<String>> map=new HashMap<>();
-       for(int i=0;i<arr.length;i++){
-   String key=GetKey(arr[i]);
-   if(!map.containsKey(key)){
-    map.put(key,new ArrayList<>());
-   }
-   map.get(key).add(arr[i]);
-       }
-       for(String x:map.keySet()){
-        res.add(map.get(x));
-       }
-       return res;
+    List<List<String>>  res=new ArrayList<>();
+
+HashMap<String,List<String>> map=new HashMap<>();
+for(int i=0;i<arr.length;i++){
+    String key=GetKey(arr[i]);
+    map.putIfAbsent(key,new ArrayList<>());
+    map.get(key).add(arr[i]);
+}
+for(String x:map.keySet()){
+    List<String> curr=new ArrayList<>();
+    for(String y:map.get(x)){
+        curr.add(y);
     }
-    public static String GetKey(String s) {
-      int [] freq=new int[26];
-      for(int i=0;i<s.length();i++){
-        char ch=s.charAt(i);
-        freq[ch-'a']++;
-      }
-      StringBuilder sb=new StringBuilder();
-      for(int i=0;i<freq.length;i++){
-        sb.append(freq[i]+" ");
-      }
-      return sb.toString();
-    }
+    res.add(curr);
+}
+return res;
+
+       }
+       public String GetKey (String s){
+        int [] freq=new int[26];
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            freq[ch-'a']++;
+        }
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<26;i++){
+            sb.append(freq[i]+" ");
+        }
+        return sb.toString();
+       }
+    
+    
 }
