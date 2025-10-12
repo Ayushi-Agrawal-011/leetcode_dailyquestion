a124f0bb98d0f92a3f0a9b65c5e891ae24632fd2
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2025-10-12 17:22:00 +0530

    Time: 92 ms (8.07%) | Memory: 57.6 MB (90.21%) - LeetSync

--- 2294-minimum-time-to-complete-trips/minimum-time-to-complete-trips.java
+++ 2294-minimum-time-to-complete-trips/minimum-time-to-complete-trips.java
@@ -1,27 +1,26 @@
 class Solution {
     public long minimumTime(int[] time, int totalTrips) {
-         long left = 1; 
-        long right = (long) 1e14; 
-        long ans=0;
-        while(left<=right){
-            long mid=left+(right-left)/2;
-            long trips=sum(time,mid);
-            if(trips>=totalTrips){
-                ans=mid;
-                right=mid-1;
-            }
-            else
-            left=mid+1;
+       long left=1;
+       long right=(long)1e14;
+       long ans=0;
+       while(left<=right){
+        long mid=left+(right-left)/2;
+        long trips=sum(time,mid);
+        if(trips>=totalTrips){
+            ans=mid;
+            right=mid-1;
         }
-        return ans;
+        else
+        left=mid+1;
+       }
+       return ans;
     }
 public long sum(int []arr,long t){
 long k=0;
 for(int i=0;i<arr.length;i++){
     k+=t/arr[i];
     if(k==1e14)
-     break;
-
+    break;
 }
 return k;
 }
