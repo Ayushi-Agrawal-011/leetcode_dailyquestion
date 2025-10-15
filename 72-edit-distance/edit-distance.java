b6dd8f174aee364c1b64fd0965405ee0df4a32c2
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2025-10-15 18:18:43 +0530

    Time: 3 ms (95.30%) | Memory: 44.9 MB (88.20%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -10,23 +10,28 @@ class Solution {
     }
 
     public static int Min_Ops(String s, String t, int i, int j, int[][] dp) {
-        if (i == s.length()) {
-            return t.length() - j;  // baki ke characters insert karne honge
-        }
-        if (j == t.length()) {
-            return s.length() - i;  // baki ke characters delete karne honge
-        }
+  if(i==s.length()){
+    return t.length()-j;
+  }
+   else if(j==t.length()){
+    return s.length()-i;
+   }
+   if(dp[i][j]!=-1)
+   return dp[i][j];
+   if(s.charAt(i)==t.charAt(j)){
+    return  dp[i][j]=Min_Ops(s,t,i+1,j+1,dp);
+   }
+   else{
+int D=Min_Ops(s,t,i+1,j,dp);
+int I=Min_Ops(s,t,i,j+1,dp);
+int R=Min_Ops(s,t,i+1,j+1,dp);
+ return  dp[i][j]=Math.min(D,Math.min(I,R))+1;
+   }
+ 
 
-        if (dp[i][j] != -1) return dp[i][j];
 
-        if (s.charAt(i) == t.charAt(j)) {
-            // agar characters match kar gaye -> koi operation nahi
-            return dp[i][j] = Min_Ops(s, t, i + 1, j + 1, dp);
-        } else {
-            int D = Min_Ops(s, t, i + 1, j, dp);      // delete
-            int R = Min_Ops(s, t, i + 1, j + 1, dp);  // replace
-            int I = Min_Ops(s, t, i, j + 1, dp);      // insert
-            return dp[i][j] = Math.min(D, Math.min(R, I)) + 1;
-        }
+
+
+        
     }
 }
