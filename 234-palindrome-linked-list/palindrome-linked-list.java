2bbc257fa81825520d0d42521d48511fb6e786cc
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2025-10-31 12:46:45 +0530

    Time: 6 ms (46.23%) | Memory: 68.6 MB (44.97%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -10,40 +10,41 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-     ListNode meet=middleNode(head);
-     System.out.println(meet.val);
-    ListNode k= reverseList(meet);
-  ListNode p=head;
-     while(k!=null){
-        if(k.val!=p.val){
-            return false;
-        }
-        k=k.next;
-        p=p.next;
-     }
-    
-     return true;   
+ListNode meet=middleNode(head);
+ListNode x=reverseList(meet);
+ListNode y=head;
+while(x!=null && y!=null){
+    if(x.val!=y.val)
+    return false;
+    x=x.next;
+    y=y.next;
+}
+return true;
+
     }
-     public ListNode middleNode(ListNode head) {
-	        ListNode slow=head;
-	        ListNode fast=head;
-	        while(fast!=null && fast.next!=null){
-	            slow=slow.next;
-	            fast=fast.next.next;
-	        }
-            
-	        return slow;
-	    }
-        public ListNode reverseList(ListNode head) {
-	        ListNode curr=head;
-	        ListNode prev=null;
-	        while(curr!=null){
-	            ListNode ahead=curr.next;
-	            curr.next=prev;
-	            prev=curr;
-	            curr=ahead;
-	        }
-	        return prev;
-	    }
-	
+        public ListNode middleNode(ListNode head) {
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return slow;
+    }
+       public ListNode reverseList(ListNode head) {
+        ListNode curr=head;
+        ListNode prev=null;
+        ListNode temp=head;
+        while(curr!=null){
+            ListNode ahead=curr.next;
+            curr.next=prev;
+          
+            prev=curr;
+            curr=ahead;
+
+        }
+        return prev;
+        
+    }
+    
 }
