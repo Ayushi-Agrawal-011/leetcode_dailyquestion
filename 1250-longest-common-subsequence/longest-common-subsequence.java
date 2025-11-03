59625e6c3acce2cec6071e8acef07a351a2a3ea2
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2025-11-03 19:47:09 +0530

    Time: 31 ms (12.43%) | Memory: 54.2 MB (5.70%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,27 +1,30 @@
 class Solution {
-    public int longestCommonSubsequence(String s1, String s2) {
-       int [][]dp=new int[s1.length()][s2.length()];
-       for(int i = 0; i < dp.length; i++) {
-    for(int j = 0; j < dp[0].length; j++) {
-        dp[i][j] = -1;
-    }
-}
-return so(s1,s2,0,0,dp);
-    }
-     public int so(String s, String t,int i,int j,int [][]dp) {
-        if(i==s.length() || j==t.length()){
-            return 0;
-        }
-        if(dp[i][j]!=-1)
-        return dp[i][j];
-        if(s.charAt(i)==t.charAt(j)){
-            return dp[i][j]=1+so(s,t,i+1,j+1,dp);
-        }
-        else{
-            int a=so(s,t,i,j+1,dp);
+    public int longestCommonSubsequence(String s, String t) {
 
-            int b=so(s,t,i+1,j,dp);
-            return dp[i][j]=Math.max(a,b);
+	    int[][] dp = new int[s.length()][t.length()];
+        for (int i = 0; i < dp.length; i++) {
+            Arrays.fill(dp[i], -1);
         }
+		return (Coin_Change(s,t,0,0,dp));
+
+    }
+    	 public static int Coin_Change(String s, String t, int i, int j,int[][]dp) {
+
+            if(i==s.length() || j==t.length()){
+                return 0;
             }
-}
+            if(dp[i][j]!=-1)
+            return dp[i][j];
+            int ans=0;
+            if(s.charAt(i)==t.charAt(j)){
+              ans=1+Coin_Change(s,t,i+1,j+1,dp);
+            }
+            else{
+                int fs=Coin_Change(s,t,i+1,j,dp);
+                               int rs=Coin_Change(s,t,i,j+1,dp);
+ans=Math.max(fs,rs);
+            }
+            dp[i][j]=ans;
+            return dp[i][j];
+         }}
+ 
