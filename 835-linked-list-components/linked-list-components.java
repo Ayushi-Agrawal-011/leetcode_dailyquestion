696c158f3b3cd26b4756e80401627d6d60b6d86e
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2025-11-03 13:21:32 +0530

    Time: 7 ms (71.12%) | Memory: 47.3 MB (7.19%) - LeetSync

---
+++ 835-linked-list-components/linked-list-components.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int numComponents(ListNode head, int[] nums) {
+      Set<Integer> set=new HashSet<>();
+        for(int i=0;i<nums.length;i++){
+            set.add(nums[i]);
+
+        }
+
+        int c=0;
+        while(head!=null){
+        if(set.contains(head.val)&& 
+    (head.next==null || !set.contains(head.next.val)))
+        c++;  
+        head=head.next;  
+        }
+
+        //System.out.println(head.val);
+       return c; 
+    }
+}
