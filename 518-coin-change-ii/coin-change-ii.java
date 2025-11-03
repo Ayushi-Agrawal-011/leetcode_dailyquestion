ab47f4828c1a4f558e6a7e56136ca31d2186f07c
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2025-11-03 19:02:17 +0530

    Time: 26 ms (14.63%) | Memory: 54 MB (17.46%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -1,23 +1,26 @@
 class Solution {
-    public int change(int amount, int[] coins) {
-    int [][]dp=new int[amount+1][coins.length];
 
-for(int []a:dp) {
-	 Arrays.fill(a, -1);
-}
-return Coin_Change(coins,amount,0,dp);
+    public int change( int amount,int[] coins) {
+	int[][] dp = new int[amount + 1][coins.length];
+		for (int[] a : dp) {
+			Arrays.fill(a, -1);
+		}
+       return c2(coins,amount,0,dp);
     }
-    public static int Coin_Change(int[]coin,int amount,int i,int[][]dp) {
-	if(amount==0)
-    return 1;
-    else if(i==coin.length)
-    return 0;
-    if(dp[amount][i]!=-1)
-    return dp[amount][i];
-    int inc=0,exc=0;
-    if(amount>=coin[i])
-     inc=Coin_Change(coin,amount-coin[i],i,dp);
-     exc=Coin_Change(coin,amount,i+1,dp);
-    return dp[amount][i]= inc+exc;
+    public int c2(int[]coins,int amount,int i,int [][]dp){
+        if(amount==0)
+        return 1;
+        if(i==coins.length)
+        return 0;
+        if(dp[amount][i]!=-1)
+        return dp[amount][i];
+        int pick=0,dopick=0;
+        if(amount>=coins[i])
+         pick=c2(coins,amount-coins[i],i,dp);
+         dopick=c2(coins,amount,i+1,dp);
+        return dp[amount][i]=pick+dopick;
     }
+
+    
+	
 }
