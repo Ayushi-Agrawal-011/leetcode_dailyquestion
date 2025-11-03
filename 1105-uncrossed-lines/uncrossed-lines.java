f4c44be8b789d32db3a02a20c5e79aa0e0fecafe
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2025-11-03 19:52:48 +0530

    Time: 5 ms (96.86%) | Memory: 45.6 MB (5.27%) - LeetSync

--- 1105-uncrossed-lines/uncrossed-lines.java
+++ 1105-uncrossed-lines/uncrossed-lines.java
@@ -1,19 +1,26 @@
 class Solution {
     public int maxUncrossedLines(int[] s1, int[] s2) {
-        int [][]dp=new int[s1.length+1][s2.length+1];
-		 for(int i = 1; i < dp.length; i++) {
-			    for(int j = 1; j < dp[0].length; j++) {
-			    	int ans=0;
-			    	  if(s1[i-1]==s2[j-1]) {
-			    		  ans=1+dp[i-1][j-1];
-			    }
-			    	  else{
-			    		  int f=dp[i-1][j];
-			    		  int t=dp[i][j-1];
-			    		  ans=Math.max(f,t);
-			    		  }
-			    	  dp[i][j]=ans;
-			    	  }}
-			    return dp[dp.length-1][dp[0].length-1];
+          int [][]dp=new int[s1.length][s2.length];
+	     for(int []a:dp) {
+	    	 Arrays.fill(a, -1);
+	}
+	return( so(s1,s2,0,0,dp));
+    }
+    public static int so(int[]s1, int[] s2,int i,int j,int [][]dp) {
+        if(i==s1.length || j==s2.length)
+        return 0;
+        if(dp[i][j]!=-1)
+        return dp[i][j];
+        int ans=0;
+      if(s1[i]==s2[j]) {
+   ans=1+so(s1,s2,i+1,j+1,dp);// if matches dono ka pointer match kro
+      }
+      else{
+        int f=so(s1,s2,i+1,j,dp);//ek baar pehle string ka pointer bdhao
+          int t=so(s1,s2,i,j+1,dp);//ek baar dusri string ka pointer bdhao
+          ans=Math.max(f,t);//dono ka max lo
+      }
+      return dp[i][j]=ans;
+     
     }
 }
