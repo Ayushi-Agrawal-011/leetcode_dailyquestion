23997633ef401e61a9dfe137b9149d92c22887e7
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2025-11-03 19:13:50 +0530

    Time: 23 ms (16.17%) | Memory: 54.3 MB (13.35%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -7,19 +7,20 @@ class Solution {
       return Coin_Change(s,t,0,0,dp);
     }
     public static int Coin_Change(String s, String t, int i, int j,int[][]dp) {
-		if (j == t.length()) {
-			return 1;
-		}
-		if (i == s.length()) {
-			return 0;
-		}
-        
-        if (dp[i][j] != -1) return dp[i][j];
-		int inc = 0, exc = 0;
-		if (s.charAt(i) == t.charAt(j)) {
-			inc = Coin_Change(s, t, i + 1, j + 1,dp);
-		}
-		exc = Coin_Change(s, t, i + 1, j,dp);
-		return dp[i][j]=inc+exc ;
+        if(j==t.length())
+        return 1;
+		if(i==s.length())
+        return 0;
+        if(dp[i][j]!=-1)
+        return dp[i][j];
+        int pick=0,dopick=0;
+        if(s.charAt(i)==t.charAt(j)){
+        pick=Coin_Change(s,t,i+1,j+1,dp);
+        }
+        dopick=Coin_Change(s,t,i+1,j,dp);
+        return dp[i][j]= pick+dopick;
+       
+
+
     }
 }
