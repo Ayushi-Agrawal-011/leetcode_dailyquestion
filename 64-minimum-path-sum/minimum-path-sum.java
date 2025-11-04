43667cded37b5f5f80184a98682ca6e4d266961d
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2025-11-04 18:27:00 +0530

    Time: 1 ms (99.76%) | Memory: 50.2 MB (5.02%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -1,23 +1,26 @@
 class Solution {
     public int minPathSum(int[][] grid) {
-       int n=grid.length;
-       int m=grid[0].length; 
-       int[][] dp = new int[n][m];
-        for(int[] num : dp)
-            Arrays.fill(num, -1);
-    return solve(grid,dp,n,m,0,0);
+        int m=grid.length;
+        int n=grid[0].length;
+       int ans=Integer.MAX_VALUE;
+       int[][]dp=new int[m][n];
+       for(int[]ar:dp){
+        Arrays.fill(ar,-1);
+       }
+     return  fn(grid,0,0,grid.length,grid[0].length,dp);
     }
-    public int solve(int [][]grid,int [][] dp,int n,int m,int i,int j){
-          if( i<0 || j<0 ||i>=n || j>=m)
-        return Integer.MAX_VALUE;
-        if(i==n-1 && j==m-1)
-        return grid[i][j];
-      
+    public int fn(int[][]grid,int i,int j,int m,int n,int [][] dp){
+        if(i<0 || j<0 || i>=m || j>=n){
+            return Integer.MAX_VALUE;
+        }
+        if(i==m-1 && j==n-1){
+            return grid[i][j];
+        }
         if(dp[i][j]!=-1)
         return dp[i][j];
-        int a=solve(grid,dp,n,m,i+1,j);
-        int b=solve(grid,dp,n,m,i,j+1);
-        return dp[i][j]=Math.min(a,b)+grid[i][j];
-      
+        int down=fn(grid,i+1,j,m,n,dp);
+        int right=fn(grid,i,j+1,m,n,dp);
+        return dp[i][j]=Math.min(down,right)+grid[i][j];
+
     }
 }
