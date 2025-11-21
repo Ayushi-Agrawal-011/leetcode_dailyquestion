88cbc1691684fdfa027e040779ba66264dc7cc5d
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2025-11-21 12:23:03 +0530

    Time: 12 ms (10.79%) | Memory: 47.4 MB (7.41%) - LeetSync

---
+++ 207-course-schedule/course-schedule.java
@@ -0,0 +1,54 @@
+class Solution {
+      private HashMap<Integer, List<Integer>> mp;
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+   Solution ts=new Solution(numCourses);
+          
+for(int i=0;i<prerequisites.length;i++){
+ts.AddEdge(prerequisites[i][1],prerequisites[i][0]);
+}
+return   ts.isCyle();
+    }
+     public Solution() {}
+     public Solution(int v){
+	        mp=new HashMap<>();
+	        for (int i=0;i<v;i++){
+	            mp.put(i,new ArrayList<>());
+	        }
+
+	    }
+	   
+	    public void AddEdge(int v1,int v2){
+	        mp.get(v1).add(v2);
+
+	    }
+	    public int [] indegree(){
+	        int [] in=new int[mp.size()];
+	        for (int v1:mp.keySet()){
+	            for (int v2:mp.get(v1)){
+	                in[v2]++;
+	            }
+	        }
+	        return in;
+	    }
+	    public boolean isCyle(){
+	        Queue<Integer> q=new LinkedList<>();
+	        int[] in=indegree();
+	        for (int i=0;i<in.length;i++){
+	            if(in[i]==0){
+	                q.add(i);
+	            }
+	        }
+	        int c=0;
+	        while(!q.isEmpty()){
+	            int r=q.poll();
+	            c++;
+//	            System.out.println(r+" ");
+	            for (int nbrs:mp.get(r)){
+	                in[nbrs]--;
+	                if(in[nbrs]==0){
+	                    q.add(nbrs);
+	                }
+	            }
+	        }
+	        return c==mp.size();
+	    }}
