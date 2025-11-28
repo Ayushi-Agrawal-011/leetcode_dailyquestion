56d9c51770547914776b7e29258181637a22e209
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2025-11-28 12:41:29 +0530

    Time: 5 ms (86.89%) | Memory: 51 MB (34.02%) - LeetSync

--- 954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.java
+++ 954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.java
@@ -1,25 +1,23 @@
 class Solution {
     public int maxSubarraySumCircular(int[] nums) {
-       int maxi=nums[0];
-       int mini=nums[0];
-       int total=0;
-       int curr_maxi=0,curr_mini=0;
-       for(int i=0;i<nums.length;i++){
-        total+=nums[i];
-        curr_maxi+=nums[i];
-        maxi=Math.max(maxi,curr_maxi);
-        if(curr_maxi<0){
-            curr_maxi=0;
-        }
-        curr_mini+=nums[i];
-        mini=Math.min(mini,curr_mini);
-        if(curr_mini>0){
-            curr_mini=0;
-        }
-
-       }
-       if(total==mini)
-       return maxi;
-       return Math.max(maxi,total-mini);
+int maxi=nums[0];
+int mini=nums[0];
+int total=0,curr_maxi=0,curr_mini=0;
+for(int i=0;i<nums.length;i++){
+    total+=nums[i];
+    curr_maxi+=nums[i];
+    curr_mini+=nums[i];
+    maxi=Math.max(maxi,curr_maxi);
+    mini=Math.min(mini,curr_mini);
+    if(curr_maxi<0){
+        curr_maxi=0;
+    }
+    if(curr_mini>0){
+        curr_mini=0;
+    }
+}
+if(total==mini)
+return maxi;
+return Math.max(maxi,total-mini);
     }
 }
