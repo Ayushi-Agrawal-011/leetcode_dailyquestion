28318ce172069daa0876faae74909599d790b07a
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2025-11-29 14:20:17 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (31.85%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,31 +1,30 @@
 class Solution {
     public int search(int[] nums, int target) {
-      
-        return binary_search(nums,target);
+      return binary_search(nums,target);
+       
     }
     	public static int binary_search(int [] arr,int item) {
-         
-		    int start=0;
-	       int last=arr.length-1;
-	    
-	       for(int mid;start<=last;) {
-	    	   mid=start+(last-start)/2;
-               if(arr[mid]==item)
-               return mid;
-	    	   if(arr[start]<=arr[mid]){
-                if(item>=arr[start] && item<arr[mid])
-                last=mid-1;
-                else
-                start=mid+1;
-               }
-               else {
-                  if(item>arr[mid] && item<=arr[last])
-                    start=mid+1;
-                else
-                   last=mid-1;
-               }
-	    	  
-	       }
-	       return -1;
+    int lo=0;
+    int hi=arr.length-1;
+    while(lo<=hi){
+        int mid=lo+(hi-lo)/2;
+        if(arr[mid]==item)
+        return mid;
+        if(arr[lo]<=arr[mid]){
+            if(item>=arr[lo] && item<=arr[mid]){
+                hi=mid-1;
+            }
+            else
+            lo=mid+1;
+        }
+        else{
+ if(item>=arr[mid] && item<=arr[hi]){
+              lo=mid+1;
+            }
+            else
+            hi=mid-1;
+        }
+    }     
+		return -1; 
 	}
 }
