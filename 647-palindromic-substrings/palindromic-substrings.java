0a3da9f72b4bf880295adfb70fde821ac3cc8297
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2025-12-01 13:06:51 +0530

    Time: 831 ms (7.55%) | Memory: 47 MB (11.62%) - LeetSync

---
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -0,0 +1,23 @@
+class Solution {
+
+    public int countSubstrings(String s) {
+        int c=0;
+        for(int i=0;i<s.length();i++){
+            for(int j=i;j<s.length();j++){
+                if(ispal(s.substring(i,j+1)))
+                c++;
+            }
+        }
+        return c;
+    }
+    public  boolean ispal(String s){
+        int i=0,j=s.length()-1;
+        while(i<j){
+            if(s.charAt(i)!=s.charAt(j))
+            return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
