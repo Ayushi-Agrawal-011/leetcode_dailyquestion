9d0ccd86842747aeab7a0a4bafaab6f83ab8cff5
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2025-12-02 22:52:51 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (10.45%) - LeetSync

--- 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.java
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.java
@@ -19,17 +19,19 @@ class Solution {
       MakeLL(result);
     }
     public TreeNode MakeLL(TreeNode root){
-        if(root==null)
-        return null;
-        if(root.left==null && root.right==null)
-        return root;
-        TreeNode left_tail=MakeLL(root.left);
-        TreeNode right_tail=MakeLL(root.right);
-        if(root.left!=null){
-            left_tail.right=root.right;
-            root.right=root.left;
-            root.left=null;
-        }
-        return right_tail!=null?right_tail:left_tail;
+     if(root==null)
+     return null;
+     if(root.left==null && root.right==null)
+     return root;
+     TreeNode left=MakeLL(root.left);
+     TreeNode right=MakeLL(root.right);
+     if(root.left!=null){
+        left.right=root.right;
+        root.right=root.left;
+        root.left=null;
+     }
+     if(right!=null)
+     return right;
+     return left;
     }
 }
