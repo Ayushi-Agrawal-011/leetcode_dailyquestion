c8c02ddaf6b48bcb0c6b0bf71392bba5997b8961
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2025-12-02 22:23:29 +0530

    Time: 8 ms (28.79%) | Memory: 67.4 MB (8.93%) - LeetSync

--- 1475-maximum-sum-bst-in-binary-tree/maximum-sum-bst-in-binary-tree.java
+++ 1475-maximum-sum-bst-in-binary-tree/maximum-sum-bst-in-binary-tree.java
@@ -13,33 +13,33 @@
  *     }
  * }
  */
+ class BSTPair{
+long maxi=Long.MIN_VALUE;
+
+long mini=Long.MAX_VALUE;
+boolean isbal=true;
+int sum=0;
+int ans=0;
+ }
 class Solution {
     public int maxSumBST(TreeNode root) {
         return helper(root).ans;
     }
      public BSTPair helper(TreeNode root) {
-	        if(root==null){
-	            return new BSTPair();
-	        }
-	     
-	        BSTPair ldp=helper(root.left);
-	          BSTPair rdp=helper(root.right);
-	            BSTPair sdp=new  BSTPair();
-	            sdp.max=Math.max(ldp.max,Math.max(rdp.max,root.val));
-	            sdp.min=Math.min(ldp.min,Math.min(rdp.min,root.val)) ;
-                sdp.sum=ldp.sum+rdp.sum+root.val;
-	            sdp.isBST=ldp.isBST && rdp.isBST && ldp.max<root.val && rdp.min>root.val;
-                if(sdp.isBST)
-                sdp.ans=Math.max(ldp.ans,Math.max(rdp.ans,sdp.sum));
-                else
-                sdp.ans=Math.max(ldp.ans,rdp.ans);
-	         return sdp;
-	}
-	class BSTPair{
-	    long min=Long.MAX_VALUE;
-	     long max=Long.MIN_VALUE;
-	     boolean isBST=true;
-         int sum=0;//bt ka sum
-         int ans=0;// max bst part ka sum
-	}
+     if(root==null)
+     return new BSTPair();
+     BSTPair ldp=helper(root.left);
+     BSTPair rdp=helper(root.right);
+     BSTPair sdp=new BSTPair();
+     sdp.maxi=Math.max(ldp.maxi,Math.max(rdp.maxi,root.val));
+     sdp.mini=Math.min(ldp.mini,Math.min(rdp.mini,root.val));
+     sdp.isbal=ldp.isbal && rdp.isbal && ldp.maxi<root.val && rdp.mini>root.val;
+      sdp.sum=ldp.sum+rdp.sum+root.val;
+      if(sdp.isbal)
+      sdp.ans=Math.max(ldp.ans,Math.max(rdp.ans,sdp.sum));
+      else
+      sdp.ans=Math.max(ldp.ans,rdp.ans);
+
+     return sdp;
+}
 }
