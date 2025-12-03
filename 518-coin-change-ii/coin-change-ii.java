9a698b1b595144b17cfb96fb429de98f720a3034
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2025-12-03 14:18:42 +0530

    Time: 25 ms (19.25%) | Memory: 53.8 MB (46.94%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -5,22 +5,29 @@ class Solution {
 		for (int[] a : dp) {
 			Arrays.fill(a, -1);
 		}
-       return c2(coins,amount,0,dp);
-    }
-    public int c2(int[]coins,int amount,int i,int [][]dp){
-        if(amount==0)
-        return 1;
-        if(i==coins.length)
-        return 0;
-        if(dp[amount][i]!=-1)
-        return dp[amount][i];
-        int pick=0,dopick=0;
-        if(amount>=coins[i])
-         pick=c2(coins,amount-coins[i],i,dp);
-         dopick=c2(coins,amount,i+1,dp);
-        return dp[amount][i]=pick+dopick;
+       return r(coins,amount,0,dp);
     }
 
+     public int r(int []coins,int amount,int i,int[][]dp){
+       if(amount ==0)
+       return 1;
     
+    if(i==coins.length ){
+        return 0;
+    }
+  
+   
+    if(dp[amount][i]!=-1)
+    return dp[amount][i];
+    int inc=0;
+    int exc=0;
+    if(amount>=coins[i]){
+        inc=r(coins,amount-coins[i],i,dp);
+    }
+exc=r(coins,amount,i+1,dp);
+return dp[amount][i]=inc+exc;
+  
+}
+   
 	
 }
