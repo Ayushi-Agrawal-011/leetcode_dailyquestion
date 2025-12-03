afc3be1a57bee5fdae83f7bb7b02ece2364bbdf0
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2025-12-03 14:59:43 +0530

    Time: 3 ms (95.64%) | Memory: 46.1 MB (36.69%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,34 +1,32 @@
 class Solution {
     public int lengthOfLIS(int[] arr) {
-
-    		int []dp=new int[arr.length];
-		int len=1;
-		dp[0]=arr[0];
-		for(int i=1;i<dp.length;i++) {
-			if(arr[i]>dp[len-1]) {
-				dp[len]=arr[i];
-				len++;
-			}
-			else {
-				int idx=BinarySearch(dp,0,len-1,arr[i]);
-				dp[idx]=arr[i];
-			}
-		}
-		return len;
+int len=1;
+int []dp=new int[arr.length];
+dp[0]=arr[0];
+for(int i=1;i<arr.length;i++){
+    if(arr[i]>dp[len-1]){
+        dp[len]=arr[i];
+        len++;
+    }
+    else{
+        int idx=bs(dp,0,len-1,arr[i]);
+        dp[idx]=arr[i];
+    }
+}
+return len;
 	}
-	public static int BinarySearch(int[]dp,int si,int ei,int item) {
-		int idx=0;
-		while(si<=ei) {
-			int mid=(si+ei)/2;
-			if(dp[mid]>=item) {
-				idx=mid;
-				ei=mid-1;
-			}
-			else {
-				si=mid+1;
-			}
-		}
-		return idx;
-	}
-
+   public int bs(int[]arr,int si,int ei,int item){
+    int idx=0;
+    while(si<=ei){
+  int mid=(si+ei)/2;
+  if(arr[mid]>=item){
+    idx=mid;
+    ei=mid-1;
+  }
+  else
+  si=mid+1;
+    }
+    return idx;
+   }
+	
 }
