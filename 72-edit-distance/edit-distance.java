0046c3212805c6a090a03f42ecb5bdaa246b0db0
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2025-12-03 14:01:25 +0530

    Time: 3 ms (95.81%) | Memory: 47.2 MB (12.50%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -1,39 +1,31 @@
 class Solution {
     public int minDistance(String s, String t) {
-        int n = s.length();
-        int m = t.length();
-        int[][] dp = new int[n][m];
-        for (int i = 0; i < n; i++) {
-            Arrays.fill(dp[i], -1);
-        }
-        return Min_Ops(s, t, 0, 0, dp);
+    int n=s.length();
+    int m=t.length();
+    int[][]dp=new int[n][m];
+    for(int[]row:dp){
+        Arrays.fill(row,-1);
     }
-
-    public static int Min_Ops(String s, String t, int i, int j, int[][] dp) {
- if(i==s.length()){
-    return t.length()-j;
- }
-  if(j==t.length()){
-    return s.length()-i;
- }
- if(dp[i][j]!=-1)
- return dp[i][j];
- if(s.charAt(i)==t.charAt(j)){
-    return dp[i][j]=Min_Ops(s,t,i+1,j+1,dp);
- }
- else{
-    int I=Min_Ops(s,t,i,j+1,dp);
-    int D=Min_Ops(s,t,i+1,j,dp);
-    int R=Min_Ops(s,t,i+1,j+1,dp);
-      return dp[i][j] = Math.min(D, Math.min(R, I)) + 1;
-
- }
-   
- 
-
-
-
-
-        
+return r(s,t,0,0,dp);
     }
+public int r(String s,String t,int i,int j,int[][]dp){
+    if(i==s.length()){
+        return t.length()-j;
+    }
+    if(j==t.length()){
+        return s.length()-i;
+    }
+    if(dp[i][j]!=-1)
+    return dp[i][j];
+    if(s.charAt(i)==t.charAt(j)){
+        return dp[i][j]=r(s,t,i+1,j+1,dp);
+    }
+    else{
+        int D=r(s,t,i,j+1,dp);
+        int I =r(s,t,i+1,j,dp);
+         int  R=r(s,t,i+1,j+1,dp);
+         return dp[i][j]=Math.min(D,Math.min(I,R))+1;
+    }
+  
+}
 }
