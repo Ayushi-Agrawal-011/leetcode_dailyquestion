877eaf102e5f0a6d7369c6164073fde4a7090cb7
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2025-12-03 13:27:14 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (27.88%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,24 +1,29 @@
 
 class Solution {
     public int rob(int[] nums) {
-         int [] dp=new int[nums.length];
-             Arrays.fill(dp,-1);
-        int n=nums.length;
-          if (n == 0) return 0;          // No houses
+       int[]dp=new int[nums.length];
+       int n=nums.length;
+        if (n == 0) return 0;          // No houses
         if (n == 1) return nums[0];
-        int a=r(nums, 0,n-2,dp);
-         int [] dp1=new int[nums.length];
-             Arrays.fill(dp1,-1);
-        int b=r(nums,1,n-1,dp1);
-        return Math.max(a,b);
+        Arrays.fill(dp,-1);
+int a= r(nums,0,nums.length-2,dp);
+Arrays.fill(dp,-1);
+int b=r(nums,1,nums.length-1,dp);
+return Math.max(a,b);
+
+
     }
 
     private int r(int[] nums, int i,int j,int[]dp) {
-        if (i >j) return 0; // correct base case
-if(dp[i]!=-1)
-return dp[i];
-        int rob = nums[i] + r(nums, i + 2,j,dp);
-        int skip = r(nums, i + 1,j,dp);
 
-        return dp[i]=Math.max(rob, skip);
-    }}
+        if(i>j)
+        return 0;
+        if(dp[i]!=-1)
+        return dp[i];
+        int rob=nums[i]+r(nums,i+2,j,dp);
+        int dorob=r(nums,i+1,j,dp);
+        return dp[i]= Math.max(rob,dorob);
+    }
+    
+    
+    }
