bfbc25cb3c45f2b2c7f8e5bf6ca799458cb44ccc
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2025-12-07 13:47:29 +0530

    Time: 46 ms (8.99%) | Memory: 46.4 MB (23.16%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -1,28 +1,29 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-         int maxLen = 0;
-        for (char ch = 'A'; ch <= 'Z'; ch++) {
-            maxLen = Math.max(maxLen, Maxlen(s, ch, k));
+int res=0;
+for(char ch='A';ch <='Z';ch++){
+res=Math.max(res,flip(s,ch,k));
+}
+  return res;  
+}
+public static  int flip(String s,char ch,int k){
+    int flips=0;
+    int ei=0,si=0,ans=0;
+    while(ei<s.length()){
+        if(s.charAt(ei)!=ch){
+            flips++;
         }
-        return maxLen;
-    
+        while(flips>k && si<=ei){
+            if(s.charAt(si)!=ch){
+            flips--;
+        }
+        si++;
+        }
+        ans=Math.max(ans,ei-si+1);
+        ei++;
+    }
+    return ans;
 }
 
-    public static int Maxlen(String s,char ch,int k) {
-	int si=0,ei=0,flip=0,ans=0;
-	while(ei<s.length())
-	{if(s.charAt(ei)!=ch)
-		flip++;
-	while(flip>k && si<=ei) {
-		if(s.charAt(si)!=ch) {
-			flip--;
-		}
-		si++;
-	}
-	ans=Math.max(ans, ei-si+1);
-	//System.out.println(ans);
-	ei++;
-	}
-	return ans;
-}
+    
 }
