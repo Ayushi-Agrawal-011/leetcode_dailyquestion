bda19b70c8b79a4d0a20808e6749c14ea081d8e8
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2025-12-12 18:57:41 +0530

    Time: 2 ms (60.24%) | Memory: 45.4 MB (7.07%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,21 +1,19 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
         List<String> ll=new ArrayList<>();
-        gp(n,0,0,ll,"");
+        fn(n,0,0,ll,"");
         return ll;
     }
-    public void  gp (int n,int open,int closed,List<String>ll,String ans){
-          if(open==n && closed==n){
+    public  void fn(int n,int open,int closed, List<String> ll,String ans){
+        if(n==open && n==closed){
             ll.add(ans);
             return;
         }
-        if(open>n || closed>open){
-           
-            return ;
-        }
-      
-        gp(n,open+1,closed,ll,ans+"(");
-        gp(n,open,closed+1,ll,ans+")");
-
+    
+        if(  n<open ||open<closed )
+        return;
+        fn(n,open+1,closed,ll,ans+"(");
+         fn(n,open,closed+1,ll,ans+")");
     }
+    
 }
