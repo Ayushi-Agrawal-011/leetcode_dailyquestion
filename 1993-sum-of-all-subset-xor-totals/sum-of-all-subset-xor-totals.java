e4085980b23a00a2139b88116f3f5111889d8fa9
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2025-12-29 14:27:30 +0530

    Time: 15 ms (8.65%) | Memory: 47.3 MB (5.99%) - LeetSync

---
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int subsetXORSum(int[] nums) {
+        List<List<Integer>> res=subsets(nums);
+        int sum=0;
+        for(List<Integer> k:res){
+int d=0;
+for(int v:k){
+    d^=v;
+}
+sum+=d;
+        }
+        return sum;
+    }
+     public List<List<Integer>> subsets(int[] nums) {
+       List<List<Integer>> res=new ArrayList<>();
+       List <Integer> cur=new ArrayList<>();
+       Combination(nums,cur,0,res);
+       return res;
+    }
+    
+	public static void Combination(int[] nums,List<Integer> cur, int idx, List<List<Integer>> res){
+        res.add(new ArrayList<>(cur));
+    for(int i=idx;i<nums.length;i++){
+    cur.add(nums[i]);
+    Combination(nums,cur,i+1,res);
+        cur.remove(cur.size()-1);
+		}}
+
+}
