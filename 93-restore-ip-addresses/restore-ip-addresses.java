88d5bf694009a40652cf1f1959a44d5dbc20f9ab
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2025-12-31 10:52:15 +0530

    Time: 9 ms (6.63%) | Memory: 53 MB (5.38%) - LeetSync

--- 93-restore-ip-addresses/restore-ip-addresses.java
+++ 93-restore-ip-addresses/restore-ip-addresses.java
@@ -1,29 +1,30 @@
 class Solution {
     public List<String> restoreIpAddresses(String s) {
-        List<String> ls=new ArrayList<>();
-        ipadd(s,0,ls,"");
-        return ls;
+       List<String> ans=new ArrayList<>();
+
+fn(s,0,ans,"");
+return ans;
         
     }
-    public void ipadd(String s,int i,List<String> ls,String ans){
+    public void fn(String s,int i,List<String> ans,String sb){
         if(i==s.length()){
-            ans=ans.substring(0,ans.length()-1);
-            if(ans.length()==s.length()+3){
-                ls.add(ans);
-            }
+            sb=sb.substring(0,sb.length()-1);
+            if(sb.length()==s.length()+3)
+            ans.add(sb);
             return;
         }
 
-        for(int in=i+1;in<=i+3&&in<=s.length();in++){
-            String sub=s.substring(i,in);
-            if(sub.length()>1&&sub.charAt(0)=='0'){
-                continue;
-            }
+        for(int j=i+1;j<=i+3 && j<=s.length();j++){
+            String sub=s.substring(i,j);
+            if(sub.length()>1 && sub.charAt(0)=='0')
+            continue;
             int n=Integer.parseInt(sub);
-            if(n>=0&&n<=255){
-                ipadd(s,in,ls,ans+sub+".");
+            if(n>=0 && n<=255){
+                fn(s,j,ans,sb+sub+".");
             }
+
 
         }
     }
+  
 }
