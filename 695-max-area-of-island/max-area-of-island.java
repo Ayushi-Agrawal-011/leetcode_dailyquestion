bfbd15abd48a11aa21488ebd08a8b8a681baed9c
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2025-12-31 11:36:35 +0530

    Time: 1 ms (100.00%) | Memory: 46.5 MB (67.64%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.java
+++ 695-max-area-of-island/max-area-of-island.java
@@ -1,28 +1,22 @@
 class Solution {
     public int maxAreaOfIsland(int[][] grid) {
-        int n=grid.length;
-        int m=grid[0].length;
         int ans=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]==1){
-                    ans=Math.max(ans,islands(grid,i,j));
-                }
-            }
+      for(int i=0;i<grid.length;i++){
+        for(int j=0;j<grid[0].length;j++){
+            if(grid[i][j]==1)
+            ans=Math.max(ans,fn(grid,i,j));
         }
-        return ans;
-        
+      }
+     return ans;   
     }
-    public int islands(int[][] grid,int i,int j){
-        if(i<0||j<0||i>=grid.length||j>=grid[0].length||grid[i][j]==0){
-            return 0;
-        }
-
-        grid[i][j]=0;
-        int d1=islands(grid,i+1,j);
-        int d2=islands(grid,i,j+1);
-        int d3=islands(grid,i-1,j);
-        int d4=islands(grid,i,j-1);
-        return d1+d2+d3+d4+1;
+    public int fn(int[][] grid,int i,int j){
+     if(i<0 || j<0 || i>=grid.length || j>=grid[0].length || grid[i][j]==0 )
+     return 0;
+     grid[i][j]=0;
+     int a=fn(grid,i-1,j);
+     int b=fn(grid,i+1,j);
+     int c=fn(grid,i,j-1);
+     int d=fn(grid,i,j+1);
+     return a+b+c+d+1;
     }
 }
