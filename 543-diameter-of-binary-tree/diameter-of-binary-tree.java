655ddb1442ff063535380b742769967d8fe0a7a0
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2026-01-02 21:20:06 +0530

    Time: 1 ms (43.74%) | Memory: 46.9 MB (80.95%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.java
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.java
@@ -25,15 +25,16 @@ class Solution {
 		}
 
 		public DiaPair diameter(TreeNode root) {
-			if(root==null)
-            return new DiaPair();
-            DiaPair ldp=diameter(root.left);
-            DiaPair rdp=diameter(root.right);
-            DiaPair sdp=new DiaPair();
-            sdp.ht=Math.max(ldp.ht,rdp.ht)+1;
-            int st=ldp.ht+rdp.ht+2;
-            sdp.dt=Math.max(st,Math.max(ldp.dt,rdp.dt));
-            return sdp;
+		if(root==null){
+        return new DiaPair();}
+        DiaPair ldp=diameter(root.left);
+        DiaPair rdp=diameter(root.right);
+        DiaPair sdp=new DiaPair();
+        sdp.ht=Math.max(ldp.ht,rdp.ht)+1;
+        int sd=ldp.ht+rdp.ht+2;
+        sdp.dt=Math.max(sd,Math.max(ldp.dt,rdp.dt));
+        return sdp;
+        
 			
 		}
 	}
