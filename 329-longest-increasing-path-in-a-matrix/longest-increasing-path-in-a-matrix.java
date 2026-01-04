35d74e6b5f7a63d1431203dec4fe66e77d45b060
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2026-01-04 16:17:04 +0530

    Time: 7 ms (97.14%) | Memory: 47.3 MB (29.43%) - LeetSync

--- 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.java
+++ 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.java
@@ -1,44 +1,31 @@
 class Solution {
-    int m, n;
-    
     public int longestIncreasingPath(int[][] matrix) {
-        m = matrix.length;
-        n = matrix[0].length;
-        int[][] dp = new int[m][n];  // memoization
-        int result = 0;
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                result = Math.max(result, dfs(matrix, i, j, dp));
+        int n=matrix.length;
+        int m=matrix[0].length;
+        int r=0;
+        int[][]dp=new int[n][m];
+         
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                r=Math.max(r,fn(matrix,dp,i,j));
             }
         }
-
-        return result;
+        return r;
+    }
+     public int fn(int[][]matrix,int[][]dp,int i,int j){
+     if(dp[i][j]!=0)
+     return dp[i][j];
+      int max = 1;
+     if(i>0 && matrix[i-1][j] >matrix[i][j])
+  max= Math.max(max,1+fn(matrix,dp,i-1,j));
+      if(j>0 && matrix[i][j-1] >matrix[i][j])
+     max= Math.max(max, 1+fn(matrix,dp,i,j-1));
+       if(i<matrix.length-1 && matrix[i+1][j] >matrix[i][j])
+  max= Math.max(max,1+fn(matrix,dp,i+1,j));
+  if(j<matrix[0].length-1 && matrix[i][j+1] >matrix[i][j])
+     max= Math.max(max, 1+fn(matrix,dp,i,j+1));
+    return dp[i][j]=max;
     }
 
-    private int dfs(int[][] matrix, int i, int j, int[][] dp) {
-        if (dp[i][j] != 0) return dp[i][j];  // already computed
 
-        int max = 1; // At least the cell itself
-
-        // Move Up
-        if (i > 0 && matrix[i-1][j] > matrix[i][j]) {
-            max = Math.max(max, 1 + dfs(matrix, i-1, j, dp));
-        }
-        // Move Down
-        if (i < m-1 && matrix[i+1][j] > matrix[i][j]) {
-            max = Math.max(max, 1 + dfs(matrix, i+1, j, dp));
-        }
-        // Move Left
-        if (j > 0 && matrix[i][j-1] > matrix[i][j]) {
-            max = Math.max(max, 1 + dfs(matrix, i, j-1, dp));
-        }
-        // Move Right
-        if (j < n-1 && matrix[i][j+1] > matrix[i][j]) {
-            max = Math.max(max, 1 + dfs(matrix, i, j+1, dp));
-        }
-
-        dp[i][j] = max; // store result
-        return max;
-    }
 }
