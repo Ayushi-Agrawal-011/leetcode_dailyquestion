c2675c3911bb44f9845e587252551a037f13298b
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2026-01-04 11:17:51 +0530

    Time: 28 ms (20.69%) | Memory: 54 MB (51.16%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,30 +1,25 @@
 class Solution {
-    public int longestCommonSubsequence(String s, String t) {
-
-	    int[][] dp = new int[s.length()][t.length()];
-        for (int i = 0; i < dp.length; i++) {
-            Arrays.fill(dp[i], -1);
+    public int longestCommonSubsequence(String text1, String text2) {
+        int n=text1.length();
+        int m=text2.length();
+        int[][]dp=new int[n][m];
+        for(int[]ar:dp){
+            Arrays.fill(ar,-1);
         }
-		return (Coin_Change(s,t,0,0,dp));
-
+return fn(text1,text2,dp,0,0);
     }
-    	 public static int Coin_Change(String s, String t, int i, int j,int[][]dp) {
-
-            if(i==s.length() || j==t.length()){
-                return 0;
-            }
-            if(dp[i][j]!=-1)
-            return dp[i][j];
-            int ans=0;
-            if(s.charAt(i)==t.charAt(j)){
-              ans=1+Coin_Change(s,t,i+1,j+1,dp);
-            }
-            else{
-                int fs=Coin_Change(s,t,i+1,j,dp);
-                               int rs=Coin_Change(s,t,i,j+1,dp);
-ans=Math.max(fs,rs);
-            }
-            dp[i][j]=ans;
-            return dp[i][j];
-         }}
- 
+    public int fn(String text1,String text2,int[][]dp,int i,int j){
+        if(i==text1.length()|| j==text2.length())
+        return 0;
+        if(dp[i][j]!=-1)
+        return dp[i][j];
+        if(text1.charAt(i)==text2.charAt(j))
+        return dp[i][j]=1+fn(text1,text2,dp,i+1,j+1);
+        else{
+            int fs=fn(text1,text2,dp,i+1,j);
+            int ss=fn(text1,text2,dp,i,j+1);
+            dp[i][j]=   Math.max(fs,ss);
+        }
+        return dp[i][j];
+    }
+}
