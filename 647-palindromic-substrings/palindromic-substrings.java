5431bd80129419e6675e8d5895eb353f996f085e
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2026-01-04 11:56:11 +0530

    Time: 820 ms (9.99%) | Memory: 47.3 MB (10.74%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.java
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -1,17 +1,17 @@
 class Solution {
-
     public int countSubstrings(String s) {
         int c=0;
-        for(int i=0;i<s.length();i++){
-            for(int j=i;j<s.length();j++){
-                if(ispal(s.substring(i,j+1)))
-                c++;
-            }
+     for(int i=0;i<s.length();i++){
+        for(int j=i+1;j<=s.length();j++){
+            if(ispal(s.substring(i,j)))
+            c++;
         }
-        return c;
+     }
+     return c;
     }
-    public  boolean ispal(String s){
-        int i=0,j=s.length()-1;
+    public boolean ispal(String s){
+        int i=0;
+        int j=s.length()-1;
         while(i<j){
             if(s.charAt(i)!=s.charAt(j))
             return false;
