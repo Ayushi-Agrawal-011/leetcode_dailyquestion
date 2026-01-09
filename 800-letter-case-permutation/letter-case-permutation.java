fbfe586647481adc71be97e31bdca879b1ad8c38
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2026-01-09 23:41:46 +0530

    Time: 5 ms (36.46%) | Memory: 53.2 MB (15.65%) - LeetSync

--- 800-letter-case-permutation/letter-case-permutation.java
+++ 800-letter-case-permutation/letter-case-permutation.java
@@ -5,22 +5,21 @@ class Solution {
         return ll;
     }
     public static void Print(String ques,String ans,List<String> ll) {
-	if(ques.length()==0){
-        ll.add(ans);
-        return;
-    }
-    char ch=ques.charAt(0);
-    if(ch>=48 && ch<=57){
-        Print(ques.substring(1),ans+ch,ll);
-    }
-    else if(ch>=65 && ch<=90){
-         Print(ques.substring(1),ans+ch,ll);
-          Print(ques.substring(1),ans+(char)(ch+32),ll);
-    }
-    else{
-         Print(ques.substring(1),ans+ch,ll);
-          Print(ques.substring(1),ans+(char)(ch-32),ll);
-    }
+if(ques.length()==0){
+    ll.add(ans);
+    return;
+}
+char ch=ques.charAt(0);
+if(ch>=48 && ch<=57)
+Print(ques.substring(1),ans+ch,ll);
+else if(ch>=65 && ch<=90){
+    Print(ques.substring(1),ans+ch,ll);
+Print(ques.substring(1),ans+(char)(ch+32),ll);
+}
+else {
+    Print(ques.substring(1),ans+ch,ll);
+Print(ques.substring(1),ans+(char)(ch -32),ll);
+}
         }
 	
 
