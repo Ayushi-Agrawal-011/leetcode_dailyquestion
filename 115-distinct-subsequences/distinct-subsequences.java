660f1165172b76657851a8708a47a25fc37ea2dd
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2026-01-09 17:11:45 +0530

    Time: 24 ms (17.61%) | Memory: 54.4 MB (41.56%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -8,23 +8,21 @@ class Solution {
     }
     
      public int r(String s,String t,int i,int j,int[][]dp){
-          if(j==t.length()){
-        return 1;
-    }
-    if(i==s.length() ){
-        return 0;
-    }
+             if(j==t.length())
+       return 1;
+       if(i==s.length())
+       return 0;
   
-   
-    if(dp[i][j]!=-1)
-    return dp[i][j];
-    int inc=0;
-    int exc=0;
-    if(s.charAt(i)==t.charAt(j)){
-        inc=r(s,t,i+1,j+1,dp);
-    }
-exc=r(s,t,i+1,j,dp);
-return dp[i][j]=inc+exc;
+       if(dp[i][j]!=-1)
+       return dp[i][j];
+       int inc=0,exc=0;
+       if(s.charAt(i)==t.charAt(j))
+    inc=r(s,t,i+1,j+1,dp);
+      
+      exc=r(s,t,i+1,j,dp);
+       
+        return dp[i][j]=inc+exc;
+       
   
 }
 }
