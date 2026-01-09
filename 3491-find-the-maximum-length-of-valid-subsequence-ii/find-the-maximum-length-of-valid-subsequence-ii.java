9f54afd4580f5149ef4fc8e4bcff466e58d8a244
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2026-01-09 12:58:50 +0530

    Time: 51 ms (37.35%) | Memory: 54.7 MB (79.94%) - LeetSync

---
+++ 3491-find-the-maximum-length-of-valid-subsequence-ii/find-the-maximum-length-of-valid-subsequence-ii.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maximumLength(int[] nums, int k) {
+        int n=nums.length;
+        int[][]dp=new int[k][n];
+        for(int[]ar:dp){
+            Arrays.fill(ar,1);
+        }
+int ans=1;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                int mod=(nums[i]+nums[j])%k;
+                dp[mod][i]=Math.max(dp[mod][i],dp[mod][j]+1);
+                ans=Math.max(ans,dp[mod][i]);
+            }
+        }
+        return ans;
+            }
+}
