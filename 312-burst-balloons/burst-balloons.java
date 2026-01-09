2967ccffa691002bb7073e74a3ec4edfc9ab8d85
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2026-01-09 15:32:43 +0530

    Time: 119 ms (18.62%) | Memory: 44.6 MB (18.38%) - LeetSync

--- 312-burst-balloons/burst-balloons.java
+++ 312-burst-balloons/burst-balloons.java
@@ -3,25 +3,25 @@ class Solution {
         int n=arr.length;
         int[]a=new int[n+2];
         a[0]=a[n+1]=1;
-        for(int i=0;i<n;i++){
-            a[i+1]=arr[i];
+        for(int i=1;i<n+1;i++){
+            a[i]=arr[i-1];
         }
         int[][]dp=new int[a.length][a.length];
-        return Burst_Coin(a,0,a.length-1,dp);
-            }
-
-    public static int Burst_Coin(int[]a,int i,int j,int[][]dp) {
-	if(i+1==j)
-    return 0;
-    if(dp[i][j]!=0)
-    return dp[i][j];
-    int ans=Integer.MIN_VALUE;
-    for(int k=i+1;k<j;k++){
-        int p=Burst_Coin(a,i,k,dp);
-       int q=Burst_Coin(a,k,j,dp);
-       int s=a[i]*a[j]*a[k];
-  ans=Math.max(ans,p+q+s);
+    
+        return fn(a,0,a.length-1,dp);
     }
-   return  dp[i][j]=ans;
-}
+    public int fn(int[]arr,int i,int j,int[][]dp){
+        if(i+1==j)
+        return 0;
+        if(dp[i][j]!=0)
+        return dp[i][j];
+        int ans=Integer.MIN_VALUE;
+        for(int k=i+1;k<j;k++){
+            int a=fn(arr,i,k,dp);
+            int b=fn(arr,k,j,dp);
+            int c=arr[i]*arr[j]*arr[k];
+            ans=Math.max(a+b+c,ans);
+        }
+        return dp[i][j]=ans;
+    }
 }
