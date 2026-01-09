942a270992e4264dcca429850652bcbcec700247
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2026-01-09 15:14:04 +0530

    Time: 3 ms (95.82%) | Memory: 46.2 MB (58.07%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,18 +1,33 @@
 class Solution {
     public int lengthOfLIS(int[] arr) {
-      int[]dp=new int[arr.length];
-      Arrays.fill(dp,1);
+     int[]dp=new int[arr.length];
+     int len =1;
+     dp[0]=arr[0];
      for(int i=1;i<arr.length;i++){
-        for(int j=i-1;j>=0 ;j--){
-            if(arr[i]>arr[j]){
-                int x=dp[j];
-                dp[i]=Math.max(dp[i],x+1);
-            }
+        if(arr[i]>dp[len-1]){
+            dp[len]=arr[i];
+            len++;
         }
-       
+        else{
+            int idx=bs(dp,0,len-1,arr[i]);
+            dp[idx]=arr[i];
+        }
      }
-      return Arrays.stream(dp).max().getAsInt();
+     return len;
 	}
+    public int bs(int[]dp,int lo,int hi,int item){
+        int ans=0;
+        while(lo<=hi){
+            int mid=lo+(hi-lo)/2;
+            if(dp[mid]>=item){
+                hi=mid-1;
+                ans=mid;
+            }
+            else
+            lo=mid+1;
+        }
+        return ans;
+    }
    
 	
 }
