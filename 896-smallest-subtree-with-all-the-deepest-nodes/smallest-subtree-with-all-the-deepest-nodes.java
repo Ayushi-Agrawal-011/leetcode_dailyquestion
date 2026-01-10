150f9517050eb79e9d82347e82113d1d10b3593d
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2026-01-10 23:21:43 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (68.65%) - LeetSync

--- 896-smallest-subtree-with-all-the-deepest-nodes/smallest-subtree-with-all-the-deepest-nodes.java
+++ 896-smallest-subtree-with-all-the-deepest-nodes/smallest-subtree-with-all-the-deepest-nodes.java
@@ -19,24 +19,21 @@ class Solution {
      return r(root);
     }
     public TreeNode r(TreeNode root){
-            
-      if(root==null)
-      return null;
+        if(root==null)
+        return null;
         int lh=ht(root.left);
-	int rh=ht(root.right);
-    if(lh>rh){
+        int rh=ht(root.right);
+        if(lh>rh)
         return r(root.left);
+        else if(rh>lh)
+        return r(root.right);
+        return root;
     }
-   else if(rh>lh)
-   return r(root.right);
-   return root;
+    public int ht(TreeNode root){
+        if(root==null)
+        return -1;
+        int lmax=ht(root.left);
+        int rmax=ht(root.right);
+        return Math.max(lmax,rmax)+1;
     }
-    public int ht(TreeNode nn) {
-	if(nn==null) {
-		return -1;
-	}
-	int lh=ht(nn.left);
-	int rh=ht(nn.right);
-	return Math.max(lh, rh)+1;
-}
 }
