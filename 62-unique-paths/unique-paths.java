90a0f4e27bcc4b10977dd342c51712d955416439
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2026-01-11 18:59:20 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (6.13%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,19 +1,21 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        int[][] memo = new int[n+1][m+1];
-        for(int[] num : memo)
-            Arrays.fill(num, -1);
-       return solve(memo,n,m,0,0);
+      int[][]dp=new int[m][n];
+      for(int[]ar:dp){
+        Arrays.fill(ar,-1);
+      }
+      return solve(dp,n,m,0,0);
     }
  
-    public int solve(int [][] memo,int n,int m,int i,int j){
-        if(i>=n || j>=m)
+    public int solve(int [][] dp,int n,int m,int i,int j){
+        if(i<0 || j<0 || i>=m || j>=n)
         return 0;
-        if(i==n-1 && j==m-1)
-          return 1;
-          if(memo[i][j]!=-1)
-          return memo[i][j];
-          memo[i][j]=solve(memo,n,m,i,j+1)+solve(memo,n,m,i+1,j);
-          return memo[i][j];
+        if(i==m-1 && j==n-1)
+        return 1;
+        if(dp[i][j]!=-1)
+        return dp[i][j];
+        int right=solve(dp,n,m,i,j+1);
+        int down=solve(dp,n,m,i+1,j);
+        return dp[i][j]=right+down;
     }
 }
