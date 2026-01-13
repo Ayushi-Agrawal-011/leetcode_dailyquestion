3d04ad2f1b78553353c015a529cbac6790432b75
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2026-01-13 15:39:31 +0530

    Time: 88 ms (5.24%) | Memory: 92.1 MB (11.49%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.java
+++ 41-first-missing-positive/first-missing-positive.java
@@ -1,42 +1,16 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
-    //  TreeSet<Integer> set=new TreeSet<>();
-    //  for(int i=0;i<nums.length;i++){
-    //     if(nums[i]>0){
-    //         set.add(nums[i]);
-    //     }
-    //  }
-    //  int i=1;
-    //  while(set.contains(i)){
-    //     i++;
-    //  }
-    //  return i; 
-//   int i=1;
-//     Arrays.sort(nums);  
-//     for(int j=0;j<nums.length;j++){
-//         if(nums[j]>0 && nums[j]==i){
-//             i++;
-//         }
-//         else if(nums[j]>0 && nums[j]>i){
-//        return i;
-//         }
-//     }
-//     // if(nums.length)
-//     if(nums[nums.length-1]<0)
-//     return 1;
-//     return nums[nums.length-1]+1;
+    TreeSet<Integer> set=new TreeSet<>();
     for(int i=0;i<nums.length;i++){
-        while(nums[i]>0 && nums[i]<nums.length &&nums[nums[i]-1]!=nums[i]){
-            int t=nums[nums[i]-1];
-            nums[nums[i]-1]=nums[i];
-            nums[i]=t;
-            
-        }
+        if(nums[i]>0)
+        set.add(nums[i]);
     }
-    for(int i=0;i<nums.length;i++){
-       if(nums[i]!=i+1)
-       return i+1;
-    }
-    return nums.length+1;
+    if(!set.contains(1))
+  return 1;
+  for(int x:set){
+    if(!set.contains(x+1))
+    return x+1;
+  }
+  return -1;
     }
 }
