26d5cde7c5794dc13229e31721e7d2fc59de1edf
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2026-01-14 12:46:20 +0530

    Time: 1 ms (99.77%) | Memory: 48.2 MB (28.34%) - LeetSync

---
+++ 260-single-number-iii/single-number-iii.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] singleNumber(int[] arr) {
+        	int xor=0;
+		for(int i=0;i<arr.length;i++) {
+			xor^=arr[i];
+		}
+		int mask=(xor&(-1*xor));//(xor &(~(xor-1))
+		int a=0;
+		for(int i=0;i<arr.length;i++) {
+			if((mask & arr[i])!=0)
+				a=a^arr[i];
+		}
+		int b=xor^a;
+		return new int[] {a,b};
+    }
+}
