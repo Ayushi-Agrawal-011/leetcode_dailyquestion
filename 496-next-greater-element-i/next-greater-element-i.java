55c70966c025192062a698b1491cbdbc1012ff4a
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2026-01-20 14:42:07 +0530

    Time: 4 ms (73.84%) | Memory: 45.6 MB (7.73%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,32 +1,35 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-      int []ab=NGE(nums2);
-      int []k=new int[nums1.length];
-      int p=0;
-      for(int i=0;i<nums1.length;i++){
-        for(int j=0;j<nums2.length;j++){
-          if(nums2[j]==nums1[i]){
-            k[p]=ab[j];
-            p++;
-            break;
-          }
+    int[]k= NGE(nums2);
+    int[]p=new int[nums1.length];
+      HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums2.length; i++) {
+            map.put(nums2[i], k[i]);
         }
-      }
-      return k;
+
+        int[] ans = new int[nums1.length];
+        for (int i = 0; i < nums1.length; i++) {
+            ans[i] = map.get(nums1[i]);
+        }
+
+        return ans;
     }
-      public static int [] NGE(int []arr) {
-		int[]ans=new int [arr.length];
-		Stack <Integer> st=new Stack<>();
-		for(int i=0;i<ans.length;i++) {
-			while(!st.isEmpty()&& arr[i]>arr[st.peek()]) {
-				ans[st.pop()]=arr[i];
-			}
-			st.push(i);
-		}
-		while(!st.isEmpty()) {
-			ans[st.pop()]=-1;
-		}
-		
-	  return ans;
+    public static int[] NGE(int[]nums){
+int[]ans=new int[nums.length];
+Stack<Integer> st=new Stack<>();
+for(int i=0;i<nums.length;i++){
+    while(!st.isEmpty()&& nums[i]>nums[st.peek()]){
+        ans[st.pop()]=nums[i];
+    }
+    st.push(i);
+}
+while(!st.isEmpty()){
+    ans[st.pop()]=-1;
+}
+// for(int i=0;i<ans.length;i++){
+
+//     System.out.println(ans[i]+" ");
+// }
+return ans;
     }
 }
