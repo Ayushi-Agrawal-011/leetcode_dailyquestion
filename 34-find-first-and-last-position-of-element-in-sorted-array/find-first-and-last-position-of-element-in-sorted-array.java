091f9cdd668b1d25c86ee713637db7af275dd09d
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2026-01-20 14:09:20 +0530

    Time: 2 ms (2.77%) | Memory: 48.3 MB (38.56%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,53 +1,30 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-       int i=0,j=nums.length/2+1;
-        int []arr=new int[2];
-        Arrays.fill(arr,-1);
-        if(nums.length==0)
-        return arr;
-        int c=0 ,d=0;
-        while(i<j){
-if(nums[i]==target && c==0){
-    arr[0]=i;
-    c=1;
-    int k=i;
-    while(k<nums.length && nums[k]==target){
-        arr[1]=k;
-        k++;
-        d=1;
-    }
-
-    
-
-}
-i++;}
-if(c==1 ){
-    return arr;
-
-}
-else{
-    i=nums.length/2+1;
-    j=nums.length;
-     while(i<j){
-if(nums[i]==target && c==0){
-    arr[0]=i;
-    c=1;
-    int k=i;
-    while(k<j && nums[k]==target){
-        arr[1]=k;
-        k++;
-        d=1;
-    }
-    
-    
-
-}
-i++;
-
-}
-
+       int start=-1;
+       int end=-1;
+       int[] y=new int[2];
+       int c=0;
+        int k=0;
+      if(nums.length!=0) 
+       {for(int i=0;i<nums.length;i++) 
+           { 
+           if(nums[i]==target && c==0) {
+               start=i;
+               c=1;
+           }
+           else if(nums[i]==target) { 
+                
+                  end=i;
+                  k=1;
+                 
+              
+               }
+           if(k==0) 
+               end=start;
+        }}
+       y[0]=start;
+       y[1]=end;
+       return y;
         
-}
-      return arr;  
     }
 }
