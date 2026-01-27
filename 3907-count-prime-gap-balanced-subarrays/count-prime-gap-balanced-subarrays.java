2d252caaa1b53aa1b09afdda852f83d8929b2c76
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2026-01-27 16:21:50 +0530

    Time: 427 ms (57.89%) | Memory: 85.3 MB (45.61%) - LeetSync

--- 3907-count-prime-gap-balanced-subarrays/count-prime-gap-balanced-subarrays.java
+++ 3907-count-prime-gap-balanced-subarrays/count-prime-gap-balanced-subarrays.java
@@ -1,50 +1,50 @@
 class Solution {
     public int primeSubarray(int[] nums, int k) {
-
-     int n=nums.length;
-     boolean[]p=new boolean[n];
-     for(int i=0;i<n;i++){
-        p[i]=isPrime(nums[i]);
-     }
-     int lastPrime = -1, secondLastPrime = -1;
-     TreeMap<Integer,Integer> map=new TreeMap<>();
-     int si=0,ei=0;
-     int c=0;
-     long ans=0;
-     while(ei<n){
-        if(p[ei]){
-             map.put(nums[ei], map.getOrDefault(nums[ei], 0) + 1);
-             c++;
-               secondLastPrime = lastPrime;
-                lastPrime = ei;
+        int n=nums.length;
+boolean[] isprime=new boolean[nums.length];
+for(int i=0;i<n;i++){
+    isprime[i]=fn(nums[i]);
+}
+int ei=0,si=0;
+int prime=-1,lastprime=-1;
+TreeMap<Integer,Integer> map=new TreeMap<>();
+int c=0;
+long ans=0;
+while(ei<n){
+    if(isprime[ei]){
+        c++;
+map.put(nums[ei],map.getOrDefault(nums[ei],0)+1);
+lastprime=prime;
+prime=ei;
+    }
+    while(!map.isEmpty() && map.lastKey()-map.firstKey()>k){
+        if(isprime[si]){
+            c--;
+            map.put(nums[si],map.getOrDefault(nums[si],0)-1);
+            if(map.get(nums[si])==0)
+            map.remove(nums[si]);
         }
-        while(!map.isEmpty() && map.lastKey() - map.firstKey() > k){
-            if(p[si]){
-                c--;
-                  map.put(nums[si], map.getOrDefault(nums[si], 0) - 1);
-                    if (map.get(nums[si]) == 0) map.remove(nums[si]);
-
-            }
-            si++;
-        }
-        if(c>=2){
-            ans+=(secondLastPrime - si + 1);
+        si++;
+    }
+    if(c>=2){
+            ans+=(lastprime - si + 1);
         }
      ei++;
-     }
-     
-    
-       return (int)ans;}
-
-    private boolean isPrime(int num) {
-        if (num <= 1) return false;
-        if (num == 2) return true;
-        if (num % 2 == 0) return false;
-
-        for (int i = 3; i * i <= num; i += 2)
-            if (num % i == 0)
-                return false;
-
-        return true;
+}
+return (int)ans;
     }
+public boolean fn(int n){
+     if (n <= 1) return false;
+    if(n==1)
+    return false;
+    if(n==2)
+    return true;
+    if(n%2==0)
+    return false;
+    for(int i=3;i*i<=n;i+=2){
+        if(n%i==0)
+        return false;
+    }
+    return true;
+}
 }
