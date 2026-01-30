ea20a9b79d9d4dd646a616c8971014bd57edacda
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2026-01-30 14:06:15 +0530

    Time: 59 ms (28.82%) | Memory: 91.6 MB (97.29%) - LeetSync

--- 2374-steps-to-make-array-non-decreasing/steps-to-make-array-non-decreasing.java
+++ 2374-steps-to-make-array-non-decreasing/steps-to-make-array-non-decreasing.java
@@ -1,24 +1,24 @@
 class Solution {
     public int totalSteps(int[] nums) {
-    int ans=0;
-    int[]dp=new int[nums.length];
-    Stack<Integer> st=new Stack<>();
-    st.push(nums.length-1);
-    for(int i=nums.length-2;i>=0;i--){
-        if(st.isEmpty() ||nums[st.peek()]>=nums[i]){
-            st.push(i);
-        }
-        else{
-            while(!st.isEmpty() && nums[st.peek()]<nums[i]){
-                dp[i]=Math.max(dp[i]+1,dp[st.peek()]);
-                st.pop();
+        int n=nums.length;
+        Stack<Integer> st=new Stack<>();
+        int[]dp=new int[n];
+        int ans=0;
+        st.push(n-1);
+        for(int i=n-2;i>=0;i--){
+            if(!st.isEmpty() && nums[st.peek()]>=nums[i]){
+                st.push(i);
             }
-//System.out.println(dp[i]);
-            ans=Math.max(ans,dp[i]);
+            else{
+                while(!st.isEmpty() && nums[st.peek()]<nums[i]){
+                    dp[i]=Math.max(dp[i]+1,dp[st.peek()]);
+                    st.pop();
+                }
+                 ans=Math.max(ans,dp[i]);
             st.push(i);
+            }
+           
         }
-       // System.out.println(st);
-    }
-    return ans;
+        return ans;
     }
 }
