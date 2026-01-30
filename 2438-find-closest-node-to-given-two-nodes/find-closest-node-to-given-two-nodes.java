91f66f9bcded6de99307aaf8324e29e84a3465b4
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2026-01-30 14:16:37 +0530

    Time: 11 ms (64.77%) | Memory: 102.8 MB (52.05%) - LeetSync

--- 2438-find-closest-node-to-given-two-nodes/find-closest-node-to-given-two-nodes.java
+++ 2438-find-closest-node-to-given-two-nodes/find-closest-node-to-given-two-nodes.java
@@ -1,10 +1,11 @@
 class Solution {
     public int closestMeetingNode(int[] edges, int node1, int node2) {
-       int[]d1=fn(edges,node1);
-       int[]d2=fn(edges,node2);
-       int node=-1;
-       int ans=Integer.MAX_VALUE;
-       for(int i=0;i<edges.length;i++){
+        int n=edges.length;
+        int[]d1=fn(edges,node1);
+        int[]d2=fn(edges,node2);
+        int ans=Integer.MAX_VALUE;
+        int node=-1;
+      for(int i=0;i<n;i++){
         if(d1[i]==-1 || d2[i]==-1)
         continue;
         int a=Math.max(d1[i],d2[i]);
@@ -12,19 +13,20 @@ class Solution {
             ans=a;
             node=i;
         }
-       }
-      
-       return node;
+
+      }
+      return node;
     }
     public int[] fn(int[]edges,int node){
-        int[]ans=new int[edges.length];
+        int n=edges.length;
+        int[]ans=new int[n];
         Arrays.fill(ans,-1);
         int cur=node;
         int d=0;
         while(cur!=-1 && ans[cur]==-1){
             ans[cur]=d;
-cur=edges[cur];
-d++;
+            cur=edges[cur];
+            d++;
         }
         return ans;
     }
