ca19468a278becc69efffcbb2d896a764a9f0bde
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2026-02-02 15:17:04 +0530

    Time: 259 ms (5.14%) | Memory: 50 MB (5.67%) - LeetSync

---
+++ 2871-double-a-number-represented-as-a-linked-list/double-a-number-represented-as-a-linked-list.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+ import java.math.BigInteger;
+class Solution {
+    public ListNode doubleIt(ListNode head) {
+         String s="";
+     while(head!=null){
+        s+=head.val;
+        head=head.next;
+     } 
+      BigInteger no = new BigInteger(s);
+        no = no.multiply(BigInteger.valueOf(2));
+        String k = no.toString();
+      ListNode dummy = new ListNode(0);
+        ListNode temp = dummy;
+
+        for (int i = 0; i < k.length(); i++) {
+            temp.next = new ListNode(k.charAt(i) - '0');
+            temp = temp.next;
+        }
+
+        return dummy.next;}
+}
