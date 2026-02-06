656aa9735f32d9af759e5b2a0a7666a7847ba352
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2026-02-06 12:24:07 +0530

    Time: 23 ms (54.58%) | Memory: 49.6 MB (74.03%) - LeetSync

--- 1072-next-greater-node-in-linked-list/next-greater-node-in-linked-list.java
+++ 1072-next-greater-node-in-linked-list/next-greater-node-in-linked-list.java
@@ -10,23 +10,20 @@
  */
 class Solution {
     public int[] nextLargerNodes(ListNode head) {
-        List<Integer> ll=new ArrayList<>();
-        while(head!=null){
-            ll.add(head.val);
-            head=head.next;
+     List<Integer> ll=new ArrayList<>();
+     while(head!=null){
+        ll.add(head.val);
+        head=head.next;
+     }
+     int n=ll.size();
+        int[]ans=new int[n];
+        Stack<Integer> st=new Stack<>();
+        for(int i=0;i<ll.size();i++){
+            while(!st.isEmpty() &&ll.get(i)>ll.get(st.peek())){
+                ans[st.pop()]=ll.get(i);
+            }
+            st.push(i);
         }
-        int n=ll.size();
-       int[]ans=new int [n];
-		Stack <Integer> st=new Stack<>();
-		for(int i=0;i<n;i++) {
-			while(!st.isEmpty()&& ll.get(i)>ll.get(st.peek())) {
-				ans[st.pop()]=ll.get(i);
-			}
-			st.push(i);
-		}
-		while(!st.isEmpty()) {
-			ans[st.pop()]=0;
-		}
-		return ans;
+        return ans;
     }
 }
