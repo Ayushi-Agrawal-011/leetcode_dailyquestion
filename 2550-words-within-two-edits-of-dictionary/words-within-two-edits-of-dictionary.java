3ce442f1a8942e68d92c0ebd22a2b674b8815d51
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2026-02-07 15:06:58 +0530

    Time: 22 ms (7.09%) | Memory: 44.6 MB (47.24%) - LeetSync

---
+++ 2550-words-within-two-edits-of-dictionary/words-within-two-edits-of-dictionary.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<String> twoEditWords(String[] q, String[] d) {
+        int k=q.length;
+        List<String> ans=new ArrayList<>();
+        for(int i=0;i<k;i++){
+            for(int j=0;j<d.length;j++){
+                int c=0;
+                for(int p=0;p<Math.min(d[j].length(),q[i].length());p++){
+                    if(d[j].charAt(p)!=q[i].charAt(p))
+                    c++;
+                 
+                }
+                if(c<=2){
+                    ans.add(q[i]);
+                    break;
+                }
+
+            }
+        }
+        return ans;
+    }
+}
