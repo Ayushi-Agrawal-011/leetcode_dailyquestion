97c22cd4ab10e0c1622ac36b0bc8ee7fae316f30
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2026-02-11 11:53:58 +0530

    Time: 9 ms (67.38%) | Memory: 47.9 MB (57.45%) - LeetSync

---
+++ 2011-maximum-value-after-insertion/maximum-value-after-insertion.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String maxValue(String n, int x) {
+        char ch=(char)(x+'0');
+        if(n.charAt(0)=='-'){
+         int i=1;
+         while(i<n.length() && n.charAt(i)-'0'<=x){
+            i++;
+         }
+         return n.substring(0,i)+ch+n.substring(i);
+        }
+        else{
+ int i=0;
+         while(i<n.length() && n.charAt(i)-'0'>=x){
+            i++;
+         }
+         return n.substring(0,i)+ch+n.substring(i);
+        }
+
+      
+    }
+}
