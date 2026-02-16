74b3241229c58d77148d7eed94d09ac144559f23
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2026-02-16 14:39:48 +0530

    Time: 19 ms (70.71%) | Memory: 101.7 MB (77.27%) - LeetSync

--- 2597-cycle-length-queries-in-a-tree/cycle-length-queries-in-a-tree.java
+++ 2597-cycle-length-queries-in-a-tree/cycle-length-queries-in-a-tree.java
@@ -1,18 +1,19 @@
 class Solution {
     public int[] cycleLengthQueries(int n, int[][] queries) {
-     int[]ans=new int[queries.length];
-     for(int i=0;i<queries.length;i++){
-        ans[i]++;
-        int a=queries[i][0];
-        int b=queries[i][1];
-        while(a!=b){
-            if(a>b)
-            a=a/2;
-            else
-            b=b/2;
-            ans[i]++;
+        int[]ans=new int[queries.length];
+        for(int i=0;i<queries.length;i++){
+            int a=queries[i][0];
+            int b=queries[i][1];
+            int c=1;
+            while(a!=b){
+          if(b>a)
+          b=b/2;
+          else
+          a=a/2;
+          c++;
+            }
+            ans[i]=c;
         }
-     }   
-     return ans;
+        return  ans;
     }
 }
