46d9520f72426dabc97a44147703a7014fa054f9
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2026-02-16 15:10:21 +0530

    Time: 12 ms (6.46%) | Memory: 70.3 MB (5.23%) - LeetSync

--- 1475-maximum-sum-bst-in-binary-tree/maximum-sum-bst-in-binary-tree.java
+++ 1475-maximum-sum-bst-in-binary-tree/maximum-sum-bst-in-binary-tree.java
@@ -15,32 +15,30 @@
  */
 class Solution {
     public int maxSumBST(TreeNode root) {
-       return fn(root).ans;
+         return (int)fn(root).ans;
     }
-    public DiaPair fn(TreeNode root){
- if(root==null)
-        return new DiaPair();
-        DiaPair ldp=fn(root.left);
-        DiaPair rdp=fn(root.right);
-        DiaPair sdp=new DiaPair();
+    public BstPair fn(TreeNode root){
+        if(root==null)
+        return new BstPair();
+        BstPair ldp=fn(root.left);
+        BstPair rdp=fn(root.right);
+        BstPair sdp=new BstPair();
         sdp.min=Math.min(ldp.min,Math.min(rdp.min,root.val));
         sdp.max=Math.max(ldp.max,Math.max(rdp.max,root.val));
+        sdp.isBst=ldp.isBst && rdp.isBst && ldp.max<root.val && rdp.min>root.val;
         sdp.sum=ldp.sum+rdp.sum+root.val;
-        sdp.isbst=ldp.isbst && rdp.isbst && ldp.max<root.val && root.val<rdp.min;
-        if(sdp.isbst){
+        if(sdp.isBst){
             sdp.ans=Math.max(ldp.ans,Math.max(rdp.ans,sdp.sum));
-
         }
         else
         sdp.ans=Math.max(ldp.ans,rdp.ans);
         return sdp;
-        
     }
-    class DiaPair{
-        boolean isbst=true;
-        int min=Integer.MAX_VALUE;
-        int max=Integer.MIN_VALUE;
-        int sum=0;
-        int ans=0;
+     class BstPair{
+    long min=Long.MAX_VALUE;
+        long max=Long.MIN_VALUE;
+        boolean isBst=true;
+        long sum=0;
+        long ans=0;
     }
 }
