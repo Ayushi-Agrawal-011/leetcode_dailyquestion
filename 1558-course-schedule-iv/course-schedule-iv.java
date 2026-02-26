9f3fe6941b4d503a72d0d121c9365a046b3a5fa4
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2026-02-26 11:23:39 +0530

    Time: 1385 ms (5.06%) | Memory: 52.1 MB (18.93%) - LeetSync

--- 1558-course-schedule-iv/course-schedule-iv.java
+++ 1558-course-schedule-iv/course-schedule-iv.java
@@ -1,37 +1,42 @@
 class Solution {
     private HashMap<Integer,List<Integer>> map;
-    public List<Boolean> checkIfPrerequisite(int numCourses, int[][] prerequisites, int[][] queries) {
-     List<Boolean> ans=new ArrayList<>();
-     map=new HashMap<>();
-        for(int i=0;i<numCourses;i++){
-            map.put(i,new ArrayList<>());
-        }
-        for(int i=0;i<prerequisites.length;i++){
-        map.get(prerequisites[i][0]).add(prerequisites[i][1]);
-        }
-        for(int i=0;i<queries.length;i++){
-            boolean res=bfs(queries[i][0],queries[i][1]);
-ans.add(res);
-
-        }
-        return ans;
-
+    public List<Boolean> checkIfPrerequisite(int numCourses, int[][] edges, int[][] queries) {
+        int n=numCourses;
+    map=new HashMap<>();
+    for(int i=0;i<n;i++){
+    map.put(i,new ArrayList<>());
     }
-    public boolean bfs(int src,int des){
-        Queue<Integer> q=new LinkedList<>();
+
+ for(int i=0;i<edges.length;i++){
+ int a=edges[i][0];
+ int b=edges[i][1];
+ map.get(a).add(b);
+    }
+List<Boolean> ans=new ArrayList<>();
+for(int i=0;i<queries.length;i++){
+int a=queries[i][0];
+int b=queries[i][1];
+boolean res=bfs(a,b);
+ans.add(res);
+        
+    }
+    return ans;
+}
+public boolean bfs(int source,int destination){
+  Queue<Integer> q=new LinkedList<>();
         HashSet<Integer> visited=new HashSet<>();
-        q.add(src);
+        q.add(source);
         while(!q.isEmpty()){
             int r=q.poll();
             if(visited.contains(r))
             continue;
-            visited.add(r);
-            if(r==des)
+            if(r==destination)
             return true;
+            visited.add(r);
             for(int nbrs:map.get(r)){
-         q.add(nbrs);
+                if(!visited.contains(nbrs))
+                q.add(nbrs);
             }
         }
         return false;
-    }
-}
+}}
