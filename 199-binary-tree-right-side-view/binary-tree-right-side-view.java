e792a937c2bba6533f6f579b562d8f6b9a6b4b97
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2026-03-09 11:15:52 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (99.51%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.java
@@ -14,20 +14,22 @@
  * }
  */
 class Solution {
+
     int maxd=0;
     public List<Integer> rightSideView(TreeNode root) {
-         List<Integer> ll=new ArrayList<>();
-rs(root,1,ll);
-    return ll;
+        List<Integer>  ans=new ArrayList<>();
+        fn(root,1,ans);
+        return ans;
     }
-    public void rs(TreeNode root,int cl,List<Integer> ll){
+    public void fn(TreeNode root,int cl,List<Integer> ans){
         if(root==null)
         return;
-        if(cl>maxd){
-          ll.add(root.val);
-          maxd=cl;
+        if(maxd<cl){
+            ans.add(root.val);
+            maxd=cl;
         }
-        rs(root.right,cl+1,ll);
-          rs(root.left,cl+1,ll);
+                fn(root.right,cl+1,ans);
+        fn(root.left,cl+1,ans);
+
     }
 }
