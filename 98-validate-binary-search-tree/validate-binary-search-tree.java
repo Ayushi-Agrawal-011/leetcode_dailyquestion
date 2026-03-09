c3acd45ded6ef66f46081a1aa0a18e49f4f9c7a8
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2026-03-09 12:10:12 +0530

    Time: 1 ms (15.86%) | Memory: 46.7 MB (6.45%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/validate-binary-search-tree.java
@@ -13,25 +13,26 @@
  *     }
  * }
  */
- class BSTPair{
-long maxi=Long.MIN_VALUE;
-
-long mini=Long.MAX_VALUE;
-boolean isbal=true;
- }
 class Solution {
     public boolean isValidBST(TreeNode root) {
-        return helper(root).isbal;
+        
+   
+        return fn(root).isbst;
     }
-    public BSTPair helper(TreeNode root) {
-     if(root==null)
-     return new BSTPair();
-     BSTPair ldp=helper(root.left);
-     BSTPair rdp=helper(root.right);
-     BSTPair sdp=new BSTPair();
-     sdp.maxi=Math.max(ldp.maxi,Math.max(rdp.maxi,root.val));
-     sdp.mini=Math.min(ldp.mini,Math.min(rdp.mini,root.val));
-     sdp.isbal=ldp.isbal && rdp.isbal && ldp.maxi<root.val && rdp.mini>root.val;
-     return sdp;
-}
+    public BstPair fn(TreeNode root){
+        if(root==null)
+        return new BstPair();
+        BstPair ldp=fn(root.left);
+        BstPair rdp=fn(root.right);
+        BstPair sdp=new BstPair();
+       sdp.min=Math.min(ldp.min,Math.min(rdp.min,root.val));
+       sdp.max=Math.max(ldp.max,Math.max(rdp.max,root.val));
+       sdp.isbst=ldp.isbst && rdp.isbst && ldp.max<root.val && root.val<rdp.min;
+       return sdp;
+    }
+    class BstPair{
+    long min=Long.MAX_VALUE;
+    long max=Long.MIN_VALUE;
+      boolean isbst=true;
+    }
 }
