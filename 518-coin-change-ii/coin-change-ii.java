8825614b4ca4f54bc7a5a1478030d7e9c6be8b05
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2026-03-10 14:49:47 +0530

    Time: 27 ms (19.48%) | Memory: 58.9 MB (5.08%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -1,33 +1,28 @@
 class Solution {
-
-    public int change( int amount,int[] coins) {
-	int[][] dp = new int[amount + 1][coins.length];
-		for (int[] a : dp) {
-			Arrays.fill(a, -1);
-		}
-       return r(coins,amount,0,dp);
+    public int change(int amount, int[] coins) {
+        
+       
+     int[][]dp=new int[amount+1][coins.length];
+        for(int[]ar:dp){
+            Arrays.fill(ar,-1);
+        }
+        return fn(coins,amount,0,dp);
     }
-
-     public int r(int []coins,int amount,int i,int[][]dp){
-       if(amount ==0)
-       return 1;
-    
-    if(i==coins.length ){
+       public int fn(int[] coin,int amount,int i,int[][]dp){
+     if(amount==0)
+     return 1;
+        if( i>=coin.length )
         return 0;
-    }
-  
-   
-    if(dp[amount][i]!=-1)
-    return dp[amount][i];
-    int inc=0;
-    int exc=0;
-    if(amount>=coins[i]){
-        inc=r(coins,amount-coins[i],i,dp);
-    }
-exc=r(coins,amount,i+1,dp);
-return dp[amount][i]=inc+exc;
-  
-}
-   
-	
+       
+      
+        if(dp[amount][i]!=-1)
+        return dp[amount][i];
+        int inc=0,exc=0;
+        if(amount>=coin[i]){
+inc=fn(coin,amount-coin[i],i,dp);
+        }
+        
+       exc=fn(coin,amount,i+1,dp);
+        return dp[amount][i]=inc+exc;
+        }
 }
