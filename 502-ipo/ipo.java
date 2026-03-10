54a5dc4a31d1b94bbc19437789f5d9bd36e1a85d
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2026-03-10 12:46:55 +0530

    Time: 94 ms (80.73%) | Memory: 131.6 MB (88.01%) - LeetSync

--- 502-ipo/ipo.java
+++ 502-ipo/ipo.java
@@ -1,32 +1,28 @@
 class Solution {
-    public int findMaximizedCapital(int k, int w, int[]profits, int[] capital) {
-       
-      int[][] arr = new int[profits.length][2];
+    public int findMaximizedCapital(int k, int w, int[] profits, int[] capital) {
+        int[][] arr = new int[profits.length][2];
 		for (int i = 0; i < arr.length; i++) {
 			arr[i][0] = capital[i];
 			arr[i][1] = profits[i];
 		}
-		return IPO(arr, k, w); 
-    }
-    public static int IPO(int[][] arr, int k, int w) {
-        Arrays.sort(arr,(a,b)->(a[0]-b[0]));//minimize capital
-        PriorityQueue<int[]>pq=new PriorityQueue<>((a,b)->(b[1]-a[1]
-        ));
-        for(int i=0;i<arr.length;){
-            while(i<arr.length && w>=arr[i][0]){
-                pq.add(arr[i]);
+        Arrays.sort(arr,(a,b)->(a[0]-b[0]));
+        PriorityQueue<int[]> pq=new PriorityQueue<>((a,b)->(b[1]-a[1]));
+        //pq.add(arr[0]);
+        for(int i=0;i<arr.length; ){
+            while(i<arr.length &&arr[i][0]<=w){
+              pq.add(arr[i]);
                 i++;
             }
-            if(pq.isEmpty() || k==0)
+            if(k==0 || pq.isEmpty())
             return w;
             w+=pq.poll()[1];
-            k--;
+                k--;
         }
         while(!pq.isEmpty()&& k>0){
-             w+=pq.poll()[1];
-            k--;
+           w+=pq.poll()[1];
+                k--; 
         }
-		
-		return w;
-	}
+        return w;
+
+    }
 }
