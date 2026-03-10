bfdfbc268369eb6d4d6e82f6b8bf878ece8b0f43
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2026-03-10 15:52:05 +0530

    Time: 122 ms (15.46%) | Memory: 44.7 MB (16.66%) - LeetSync

--- 312-burst-balloons/burst-balloons.java
+++ 312-burst-balloons/burst-balloons.java
@@ -1,27 +1,29 @@
 class Solution {
-    public int maxCoins(int[] arr) {
-        int n=arr.length;
-        int[]a=new int[n+2];
-        a[0]=a[n+1]=1;
-        for(int i=1;i<n+1;i++){
-            a[i]=arr[i-1];
-        }
-        int[][]dp=new int[a.length][a.length];
-    
-        return fn(a,0,a.length-1,dp);
+    public int maxCoins(int[] nums) {
+      int n = nums.length;
+
+        int[] a = new int[n + 2];
+        a[0] = 1;
+        a[n + 1] = 1;
+      for(int i=0;i<nums.length;i++){
+        a[i+1]=nums[i];
+      }
+       int[][]dp=new int[a.length][a.length]; 
+       return fn(a,0,a.length-1,dp);
     }
-    public int fn(int[]arr,int i,int j,int[][]dp){
-        if(i+1==j)
-        return 0;
-        if(dp[i][j]!=0)
-        return dp[i][j];
-        int ans=Integer.MIN_VALUE;
-        for(int k=i+1;k<j;k++){
-            int a=fn(arr,i,k,dp);
-            int b=fn(arr,k,j,dp);
-            int c=arr[i]*arr[j]*arr[k];
-            ans=Math.max(a+b+c,ans);
-        }
-        return dp[i][j]=ans;
+    public int fn(int[]a,int i,int j,int[][]dp){
+
+if(i+1==j)
+return 0;
+if(dp[i][j]!=0)
+return dp[i][j];
+int ans=Integer.MIN_VALUE;
+for(int k=i+1;k<j;k++){
+    int p=fn(a,i,k,dp);
+    int q=fn(a,k,j,dp);
+    int self=a[i]*a[j]*a[k];
+    ans=Math.max(ans,p+q+self);
+}
+return dp[i][j]=ans;
     }
 }
