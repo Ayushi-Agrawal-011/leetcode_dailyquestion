67466b8f4adf24522587b9fa959d9f6793cbaf88
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2026-03-12 15:34:37 +0530

    Time: 8 ms (7.11%) | Memory: 47.5 MB (16.69%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.java
+++ 801-is-graph-bipartite/is-graph-bipartite.java
@@ -1,4 +1,27 @@
 class Solution {
+    public boolean isBipartite(int[][] graph) {
+     Queue<BipartitePair> q=new LinkedList<>();
+     HashMap<Integer,Integer> visited=new HashMap<>();
+    for(int i=0;i<graph.length;i++){
+        if(visited.containsKey(i))
+        continue;
+        q.add(new BipartitePair(i,0));
+        while(!q.isEmpty()){
+            BipartitePair rp=q.poll();
+            if(visited.containsKey(rp.vtx)){
+                if(rp.dist!=visited.get(rp.vtx))
+                return false;
+                continue;
+            }
+            visited.put(rp.vtx,rp.dist);
+            for(int nbrs:graph[rp.vtx]){
+                if(!visited.containsKey(nbrs))
+                q.add(new BipartitePair(nbrs,rp.dist+1));
+            }
+        }
+    }
+    return true;
+    }
     class BipartitePair{
 	
 	int vtx;
@@ -9,40 +32,4 @@ class Solution {
 	}
 	
 }
-    public boolean isBipartite(int[][] graph) {
-        
-    
-     int c=0;
-			Queue<BipartitePair> q=new LinkedList<>();
-			HashMap<Integer,Integer> visited=new HashMap<>();
-			for(int i=0;i<graph.length;i++) {
-			if(visited.containsKey(i)) {
-				continue;
-			}
-			q.add(new BipartitePair(i,0));
-			while(!q.isEmpty()) {
-				//1-remove
-			BipartitePair rp=q.poll();
-				//2-ignore if aldready visited
-			if(visited.containsKey(rp.vtx)) {
-				if(visited.get(rp.vtx)!=rp.dist) {
-					return false;
-				}
-				continue;
-			}
-				//3-marked visited
-			visited.put(rp.vtx, rp.dist);
-				//4-self work
-				//5-addd unvisited
-			for(int nbrs:graph[rp.vtx]) {
-				if(!visited.containsKey(nbrs)) {
-					q.add(new BipartitePair(nbrs,rp.dist+1));
-				}
-			}
-				
-			}
-			
-			}
-			return true;
-    }
 }
