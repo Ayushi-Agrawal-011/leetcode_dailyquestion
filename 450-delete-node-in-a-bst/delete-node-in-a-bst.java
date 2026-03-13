86e95203af1f9defc17ea753fcb94e78065171a8
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2026-03-13 15:08:56 +0530

    Time: 0 ms (100.00%) | Memory: 47.7 MB (31.83%) - LeetSync

--- 450-delete-node-in-a-bst/delete-node-in-a-bst.java
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.java
@@ -15,33 +15,29 @@
  */
 class Solution {
     public TreeNode deleteNode(TreeNode root, int key) {
-      if(root==null)
-      return null;
-      if(root.val<key)
-      root.right= deleteNode(root.right,key);
-
-      else if(root.val>key) {
-        root.left=deleteNode(root.left,key);
-      }
-      else{
-        if(root.left==null)
+        if(root==null)
+        return null;
+        if(root.val<key)
+        root.right=deleteNode(root.right,key);
+       else if(root.val>key){
+root.left=deleteNode(root.left,key);
+        }
+        else{
+            if(root.left==null)
 	                return root.right;
 	                else if(root.right==null)
 	                return root.left;
-	// 2child
-             else{
-         int min=min(root.right);
-	    root.right=deleteNode(root.right,min);
-	    root.val=min;
-      }}
-
-      return root;
-    }
-    public int min(TreeNode root){
-        if(root==null){
-            return Integer.MAX_VALUE;
+                    else{
+            int min=min(root.right);
+            root.right=deleteNode(root.right,min);
+            root.val=min;
+                    }
         }
-        int l=min(root.left);
-        return Math.min(l,root.val);
-    }
+        return root;
+      }
+      public int min (TreeNode root){
+        if(root==null)
+        return Integer.MAX_VALUE;
+        return Math.min(root.val,min(root.left));
+      }
 }
