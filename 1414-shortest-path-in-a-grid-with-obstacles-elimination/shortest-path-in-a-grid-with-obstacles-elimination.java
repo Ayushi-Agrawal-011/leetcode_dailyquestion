b41cd138470a1196d1d194236c4ce4978249a7a2
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2026-03-16 14:45:21 +0530

    Time: 72 ms (12.37%) | Memory: 47.6 MB (46.24%) - LeetSync

--- 1414-shortest-path-in-a-grid-with-obstacles-elimination/shortest-path-in-a-grid-with-obstacles-elimination.java
+++ 1414-shortest-path-in-a-grid-with-obstacles-elimination/shortest-path-in-a-grid-with-obstacles-elimination.java
@@ -1,48 +1,49 @@
 class Solution {
-      int[][] dir = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
     public int shortestPath(int[][] grid, int k) {
-        int m=grid.length;
-        int n=grid[0].length;
+        int n=grid.length;
+        int m=grid[0].length;
+       PriorityQueue<Pair> pq=new PriorityQueue<>((a,b)->(a.steps-b.steps));
+       boolean[][][] visited = new boolean[n][m][k+1];
+       pq.add(new Pair(0,0,k,0));
+       int[][]dirs={{1,0},{-1,0},{0,-1},{0,1}};
+       while(!pq.isEmpty()){
+        Pair rp=pq.poll();
+        if(rp.k<0 )
+        continue;
+       if(rp.r==n-1 && rp.c==m-1)
+       return rp.steps;
+       for(int []d:dirs){
+        int nr=rp.r+d[0];
+        int nc=rp.c+d[1];
+        if(nr<0 || nc<0 || nr>=n || nc>=m)
+continue;
     
-        Queue<Pair> q=new LinkedList<>();
-         boolean[][][] visited = new boolean[m][n][m*n];
-       
+    int nk=-1;
+    if(grid[nr][nc]==1)
+     nk=rp.k-1;
+    else
+      nk=rp.k;
+      if(nk < 0) 
+    continue;
+      if(!visited[nr][nc][nk]){
+        visited[nr][nc][nk]=true;
+     pq.add(new Pair(nr,nc,nk,rp.steps+1));
+      }
+       }
 
-      q.add(new Pair(0,0,0,0));
-        while(!q.isEmpty()){
-            Pair rp=q.poll();
-         
-            if(rp.row==m-1 && rp.col==n-1)
-            return rp.steps;
-         
-           for(int[]d:dir){
-            int nr=rp.row+d[0];
-            int nc=rp.col+d[1];
-            
-                if (nr < 0 || nc < 0 || nr >= m || nc >= n)
-                    continue;
-         int nk=rp.k+grid[nr][nc];
-         if(k>=nk && !visited[nr][nc][nk]){
-            visited[nr][nc][nk]=true;
-            q.add(new Pair(nr,nc,rp.steps+1,nk));
-         }
-             
-
-           }
-
-        }
-return -1;
+       } 
+       return -1;
     }
     class Pair{
-        int row;
-        int col;
-        int steps;
-        int k;
-        public Pair(int row,int col,int steps,int k){
-            this.row=row;
-            this.col=col;
-            this.steps=steps;
-            this.k=k;
-        }
+int r;
+int c;
+int k;
+int steps;
+public Pair(int r,int c,int k,int steps){
+    this.r=r;
+    this.c=c;
+    this.k=k;
+    this.steps=steps;
+}
     }
 }
