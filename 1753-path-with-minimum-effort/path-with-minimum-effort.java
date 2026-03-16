5ac1e9e04b0d78d1ce7d76534dc21c2853ea895d
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2026-03-16 12:58:08 +0530

    Time: 38 ms (92.04%) | Memory: 47.9 MB (53.84%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.java
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.java
@@ -1,9 +1,7 @@
 class Solution {
     public int minimumEffortPath(int[][] heights) {
-        int n=heights.length;
-        int m=heights[0].length;
-        if (heights.length == 1 && heights[0].length == 1)
-    return 0;
+     int n=heights.length;
+     int m=heights[0].length;
          int[] row={1,-1,0,0};
         int[] col={0,0,1,-1};
          int[][] dis=new int[n][m];
@@ -11,33 +9,28 @@ class Solution {
             Arrays.fill(dis[i],Integer.MAX_VALUE);
         }
          int ans=Integer.MAX_VALUE;
-         PriorityQueue<Pair> q =
+         PriorityQueue<Pair> pq =
     new PriorityQueue<>((a,b) -> a.diff - b.diff);
-    
-        q.add(new Pair(0,0,0));
-        while(!q.isEmpty()){
-            Pair rp=q.poll();
-            if(rp.r==n-1 && rp.c==m-1)
-       ans=Math.min(ans,rp.diff);
-            for(int i=0;i<4;i++){
-        int nr=rp.r+row[i];
-        int nc=rp.c+col[i];
-        if(nr<0 || nc<0 || nr>=n || nc>=m){
+    pq.add(new Pair(0,0,0)) ;
+    while(!pq.isEmpty()){
+        Pair rp=pq.poll();
+        if(rp.r==n-1 && rp.c==m-1)
+        return rp.diff;
+        for(int i=0;i<4;i++){
+            int nr=rp.r+row[i];
+            int nc=rp.c+col[i];
+              if(nr<0 || nc<0 || nr>=n || nc>=m){
        continue;
             }
-            int nd = Math.max(rp.diff,
-        Math.abs(heights[rp.r][rp.c] - heights[nr][nc]));
-
+            int nd=Math.max(rp.diff,Math.abs(heights[nr][nc]-heights[rp.r][rp.c]));
             if(nd<dis[nr][nc]){
                 dis[nr][nc]=nd;
-
-                q.add(new Pair(nr,nc,nd));
-
+                pq.add(new Pair(nr,nc,nd));
             }
         }
-      
     }
-      return dis[n-1][m-1];}
+     return dis[n-1][m-1];
+    }
     class Pair{
         int r;
         int c;
@@ -46,7 +39,6 @@ class Solution {
             this.r=r;
             this.c=c;
             this.diff=diff;
-
         }
     }
 }
