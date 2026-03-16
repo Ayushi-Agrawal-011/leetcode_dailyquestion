9d534a9b05965501d52695d6adcd89be65f3e2ac
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2026-03-16 11:06:34 +0530

    Time: 30 ms (8.76%) | Memory: 50.3 MB (20.68%) - LeetSync

--- 744-network-delay-time/network-delay-time.java
+++ 744-network-delay-time/network-delay-time.java
@@ -1,62 +1,42 @@
 class Solution {
+    HashMap<Integer,HashMap<Integer,Integer>> map;
     public int networkDelayTime(int[][] times, int n, int k) {
-      HashMap<Integer,HashMap<Integer,Integer>> map=new HashMap<>();
-	for(int i=1;i<=n;i++) {
-					if(!map.containsKey(i))
-			map.put(i,new HashMap<>());
-         
-	}  
-    for(int i=0;i<times.length;i++) {
-					
-			int a=times[i][0];
+        map=new HashMap<>();
+        for(int i=1;i<=n;i++){
+            map.put(i,new HashMap<>());
+        }
+        for(int i=0;i<times.length;i++){
+            int a=times[i][0];
             int b=times[i][1];
-            int cost=times[i][2];
-            map.get(a).put(b,cost);
-         
-	} 
-    int ans=0;
-    PriorityQueue<DijkstraPair> pq=new PriorityQueue<>((a,b)->a.cost-b.cost);
-		HashSet<Integer> visited=new HashSet<>();
-		pq.add(new  DijkstraPair(k,0));
-        int c=0;
-		while(!pq.isEmpty()) {
-			//1-remove
-			 DijkstraPair rp=pq.poll();
-             
+            int w=times[i][2];
+            map.get(a).put(b,w);
+        }
+        PriorityQueue<Pair> pq=new PriorityQueue<>((a,b)->(a.time-b.time));
+        pq.add(new Pair(k,0));
+        int c=0,ans=0;
+         boolean[] visited=new boolean[n+1];
+        while(!pq.isEmpty()){
+Pair rp=pq.poll();
+if(visited[rp.vtx])
+continue;
+visited[rp.vtx]=true;
+c++;
+ans=Math.max(ans,rp.time);
+for(int nbrs:map.get(rp.vtx).keySet()){
+    int cost=map.get(rp.vtx).get(nbrs);
+    pq.add(new Pair(nbrs,rp.time+cost));
+}
+        }
+        return c==n?ans: -1;
+    }
+    class Pair{
+        int vtx;
+       
+        int time;
+        public Pair(int vtx,int time){
+            this.vtx=vtx;
+            this.time=time;
           
-			 //2-ignore if visited
-			 if(visited.contains(rp.vtx))
-				 continue;
-			 //3-marked visited
-			 visited.add(rp.vtx);
-			 //4-self work
-			 c++;
-              ans=Math.max(ans,rp.cost);
-			 //5-add nbrs
-     for(int nbrs:map.get(rp.vtx).keySet()) {
-    	 if(!visited.contains(nbrs)) {
-    		 int cost=map.get(rp.vtx).get(nbrs);
-    		 pq.add(new DijkstraPair(nbrs,rp.cost+cost));
-    		 
-    	 }
-    	 
-     }}
-     if(c!=n)
-  return -1;
-
-     return ans;
+        }
     }
-    class DijkstraPair{
-  int vtx;
-
-  int cost;
-  public  DijkstraPair(int vtx,int cost) {
-	  this.vtx=vtx;
-	
-	  this.cost=cost;
-  }
-  
-
-    }
-
 }
