7cae842a03f8db17fc8607d09892d7b4f144102b
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2026-03-17 11:08:17 +0530

    Time: 35 ms (9.84%) | Memory: 63.4 MB (35.00%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.java
+++ 820-find-eventual-safe-states/find-eventual-safe-states.java
@@ -1,38 +1,36 @@
 class Solution {
     public List<Integer> eventualSafeNodes(int[][] graph) {
-       int n=graph.length;
-       List<List<Integer>> alist=new ArrayList<>();
-       for(int i=0;i<n;i++){
-        alist.add(new ArrayList<>());
-       }
-       int[]outdegree=new int[n];
-       for(int i=0;i<n;i++){
-        for(int nbrs:graph[i]){
-           alist.get(nbrs).add(i);
-           outdegree[i]++;
+        HashMap<Integer,List<Integer>> map=new HashMap<>();
+        int n=graph.length;
+        int[]out=new int[n];
+         for(int i=0;i<n;i++){
+            map.put(i,new ArrayList<>());
+         }
+        for(int i=0;i<n;i++){
+           
+             for(int j=0;j<graph[i].length;j++){
+                map.get(graph[i][j]).add(i);
+                out[i]++;
+            }
         }
-       }
-       List<Integer> safenodes=new ArrayList<>();
-       Queue<Integer> q = new LinkedList<>();
-		
-		for (int i = 0; i < outdegree.length; i++) {
-			if (outdegree[i] == 0) {
-				q.add(i);
-              
-			}
-		}
-		while (!q.isEmpty()) {
-			int r = q.poll();
-		safenodes.add(r);
-			for(int nbrs:alist.get(r)) {
-				outdegree[nbrs]--;
-				if(outdegree[nbrs]==0) {
-					q.add(nbrs);
-				}
-			}
+        Queue<Integer> q=new LinkedList<>();
+        for(int i=0;i<n;i++){
+            if(out[i]==0)
+            q.add(i);
+        }
+        List<Integer> ans=new ArrayList<>();
+        while(!q.isEmpty()){
+            int r=q.poll();
+            ans.add(r);
+            for(int nbrs:map.get(r)){
+                out[nbrs]--;
+                if(out[nbrs]==0)
+                q.add(nbrs);
+            }
 
-		}
-         Collections.sort(safenodes);
-        return safenodes;
+        }
+        Collections.sort(ans);
+        return ans;
+        
     }
 }
