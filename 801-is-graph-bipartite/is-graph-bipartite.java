60029643ee7409f6edbf9084cb27385ee427945d
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2026-03-17 10:52:59 +0530

    Time: 15 ms (6.99%) | Memory: 46.9 MB (96.18%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.java
+++ 801-is-graph-bipartite/is-graph-bipartite.java
@@ -1,35 +1,42 @@
 class Solution {
     public boolean isBipartite(int[][] graph) {
-     Queue<BipartitePair> q=new LinkedList<>();
-     HashMap<Integer,Integer> visited=new HashMap<>();
-    for(int i=0;i<graph.length;i++){
-        if(visited.containsKey(i))
-        continue;
-        q.add(new BipartitePair(i,0));
-        while(!q.isEmpty()){
-            BipartitePair rp=q.poll();
-            if(visited.containsKey(rp.vtx)){
-                if(rp.dist!=visited.get(rp.vtx))
-                return false;
-                continue;
+        int n=graph.length;
+        HashMap<Integer,List<Integer>> map=new HashMap<>();
+        for(int i=0;i<n;i++){
+            map.put(i,new ArrayList<>());
+            for(int j=0;j<graph[i].length;j++){
+                map.get(i).add(graph[i][j]);
             }
-            visited.put(rp.vtx,rp.dist);
-            for(int nbrs:graph[rp.vtx]){
-                if(!visited.containsKey(nbrs))
-                q.add(new BipartitePair(nbrs,rp.dist+1));
+        }
+        Queue<Pair> q=new LinkedList<>();
+HashMap<Integer,Integer> visited=new HashMap<>();
+for(int src:map.keySet()){
+    if(visited.containsKey(src))
+    continue;
+    q.add(new Pair(src,0));
+    while(!q.isEmpty()){
+        Pair rp=q.poll();
+        if(visited.containsKey(rp.vtx)){
+            if(visited.get(rp.vtx)!=rp.dist)
+            return false;
+            continue;
+        }
+        visited.put(rp.vtx,rp.dist);
+        for(int nbrs:map.get(rp.vtx)){
+            if(!visited.containsKey(nbrs)){
+                q.add(new Pair(nbrs,rp.dist+1));
             }
         }
     }
-    return true;
-    }
-    class BipartitePair{
-	
-	int vtx;
-	int dist;
-	public BipartitePair(int vtx,int dist) {
-		this.vtx=vtx;
-		this.dist=dist;
-	}
-	
 }
+        return true;
+    }
+    class Pair{
+        int vtx;
+        int dist;
+        public Pair(int vtx,int dist){
+            this.vtx=vtx;
+            this.dist=dist;
+        }
+    }
 }
