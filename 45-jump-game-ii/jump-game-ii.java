122be0cb257a1ac56eeea2eee97da376ecfb053a
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2026-03-18 11:26:33 +0530

    Time: 168 ms (5.11%) | Memory: 48.8 MB (5.73%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.java
+++ 45-jump-game-ii/jump-game-ii.java
@@ -1,18 +1,40 @@
 class Solution {
+    int ans=Integer.MAX_VALUE;
     public int jump(int[] nums) {
-        int n=nums.length;
-      int maxReach=0;
-      int c=0;
-      int endjump=0;
-      for(int i=0;i<n-1;i++){
-       
-        maxReach=Math.max(maxReach,i+nums[i]);
-        if(i==endjump){
-            c++;
-            endjump=maxReach;
+        
+      Integer[] dp = new Integer[nums.length];
+      Boolean[] dp2 = new Boolean[nums.length];
+      fn(nums,0,dp,dp2);
+      return dp[0];
+        
+    }
+    public boolean fn(int[]nums,int i,Integer[]dp, Boolean[] dp2){
+
+if(i>=nums.length){
+   
+return false;
+}
+if(i==nums.length-1){
+     dp[i]=0;
+return dp2[i]=true;
+}
+int min=Integer.MAX_VALUE;
+boolean can =false;
+if (dp2[i] != null) return dp2[i];
+ for (int j = 1; j <= nums[i]; j++) {
+        if (fn(nums, i + j,dp,dp2)) {
+            min=Math.min(min,dp[i+j]+1);
+            can=true;
         }
-       
-      }  
-      return c;
+
+    }
+     if (can) {
+            dp[i] = min;
+            return dp2[i] = true;
+        } else {
+            return dp2[i] = false;
+        }
+     
+
     }
 }
