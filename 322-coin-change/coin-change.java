3b8c3cead3857fb1c5c21a93a62b783c9ec678b6
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2026-03-19 11:15:49 +0530

    Time: 53 ms (6.21%) | Memory: 47.9 MB (6.30%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,28 +1,28 @@
 class Solution {
-
     public int coinChange(int[] coins, int amount) {
-	int[][] dp = new int[amount + 1][coins.length];
-		for (int[] a : dp) {
-			Arrays.fill(a, -1);
-		}
-          int ans = c2(coins, amount, 0, dp);
-     return ans >= 1e9 ? -1 : ans;
-
+        int[][]dp=new int[amount+1][coins.length];
+        for(int[]ar:dp){
+            Arrays.fill(ar,-1);
+        }
+    int ans= fn(coins,0,amount,dp);
+    return ans>=Integer.MAX_VALUE?-1:ans;
     }
-    public int c2(int[]coins,int amount,int i,int [][]dp){
+    public int fn(int[]coins,int i,int amount,int[][]dp){
         if(amount==0)
         return 0;
-        if(i==coins.length)
-        return (int)1e9;
+        if(i>=coins.length)
+        return Integer.MAX_VALUE;
         if(dp[amount][i]!=-1)
         return dp[amount][i];
-        int pick=(int)1e9,dopick=0;
-        if(amount>=coins[i])
-         pick=1+c2(coins,amount-coins[i],i,dp);
-         dopick=c2(coins,amount,i+1,dp);
-        return dp[amount][i]=Math.min(pick,dopick);
-    }
+        int inc=Integer.MAX_VALUE,exc=0;
+        if(amount>=coins[i]){
+            int res=fn(coins,i,amount-coins[i],dp);
+            if(res!=Integer.MAX_VALUE)
+            inc=1+res;
+            
+        }
+       exc= fn(coins,i+1,amount,dp); 
+      return dp[amount][i]=Math.min(inc,exc);
 
-    
-	
+    }
 }
