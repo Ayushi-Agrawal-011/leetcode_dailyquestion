326882e53d6d0f035fba6efb861f4fb2c60888c4
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2026-05-22 16:01:03 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (97.61%) - LeetSync

---
+++ 274-h-index/h-index.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        int ans=0;
+        int lo=0;
+        int high=1000;
+while(lo<=high){
+    int mid=lo+(high-lo)/2;
+    if(isitpossible(citations,mid)){
+        ans=mid;
+        lo=mid+1;
+    }
+    else
+    high=mid-1;
+}
+return ans;
+    }
+    public boolean isitpossible(int[]citations,int mid){
+        int c=0;
+        for(int i=0;i<citations.length;i++){
+           if(citations[i]>=mid)
+           c++;
+            
+        }
+        return c>=mid;
+    }
+}
