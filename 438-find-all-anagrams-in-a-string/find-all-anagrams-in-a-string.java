fab66b54dbb0a5888fd5c057e83c8053055a90f1
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2026-05-23 15:16:24 +0530

    Time: 61 ms (21.07%) | Memory: 48.6 MB (5.98%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -1,39 +1,35 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-     
-       HashMap<Character,Integer> map=new HashMap<>();
-List<Integer> ans=new ArrayList<>();
-       HashMap<Character,Integer> mp=new HashMap<>();
-            if(s.length() < p.length()) return ans;
-       for(int i=0;i<p.length();i++){
-        char ch=p.charAt(i);
-        mp.put(ch,mp.getOrDefault(ch,0)+1);
-         char a=s.charAt(i);
-        map.put(a,map.getOrDefault(a,0)+1);
-       }
-       
-
+          HashMap<Character,Integer> map=new HashMap<>();
+           HashMap<Character,Integer> mp=new HashMap<>();
+if(s.length()<p.length())
+return new ArrayList<>();
+           for(int i=0;i<p.length();i++){
+            mp.put(p.charAt(i),mp.getOrDefault(p.charAt(i),0)+1);
+             map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0)+1);
+           }
+           List<Integer> res=new ArrayList<>();
+       int ei=p.length(),si=0,ans=0;
        if(map.equals(mp))
-       ans.add(0);
-       int ei=p.length();
-       int si=0;
+res.add(0);
+       ans++;
        while(ei<s.length()){
         char ch=s.charAt(ei);
         map.put(ch,map.getOrDefault(ch,0)+1);
-        char a=s.charAt(si);
-        map.put(a,map.getOrDefault(a,0)-1);
-        if(map.get(a)==0){
-        map.remove(a);
-        }
-        si++;
+       
+            char ch1=s.charAt(si);
+             map.put(ch1,map.getOrDefault(ch1,0)-1);
+             if(map.get(ch1)==0){
+                map.remove(ch1);
+             }
+             si++;
         
-        if(map.equals(mp)){
-            ans.add(si);
-
-        }
-ei++;
-
-       }
-return ans;
+        if(map.equals(mp))
+        res.add(si);
+        ans++;
+   
+        ei++;
+       } 
+       return res;
     }
 }
