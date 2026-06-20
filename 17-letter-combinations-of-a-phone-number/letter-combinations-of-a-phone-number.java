1bb9dd387514644ee8710dd2edc98055206da08b
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2026-06-20 14:34:22 +0530

    Time: 3 ms (34.29%) | Memory: 49.2 MB (7.46%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -1,20 +1,19 @@
 class Solution {
-     static String[] key = { "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
+    static String[] key = { "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
     public List<String> letterCombinations(String s) {
-      List<String>  ll=new ArrayList<>();
-      fn(s,"",ll);
-      return ll;
+        List<String> ans=new ArrayList<>();
+        fn(s,ans,"");
+        return ans;
     }
-  public void fn(String ques,String ans,List<String> ll){
-    if(ques.length()==0){
-        ll.add(ans);
-        return;
+    public static void fn(String s,  List<String> ll,String ans){
+        if(s.length()==0){
+            ll.add(ans);
+            return;
+        }
+        char ch=s.charAt(0);
+        String gets=key[ch-'0'];
+        for(int i=0;i<gets.length();i++){
+            fn(s.substring(1),ll,ans+gets.charAt(i));
+        }
     }
-    char ch=ques.charAt(0);
-String v=key[ch-'0'];
-for(int i=0;i<v.length();i++){
-    fn(ques.substring(1),ans+v.charAt(i),ll);
-}
-
-  } 
 }
