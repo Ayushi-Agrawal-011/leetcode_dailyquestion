921e08ecb62e15d2dced7a1a4ed93c5c121fa1f4
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2026-06-23 19:48:37 +0530

    Time: 3 ms (30.01%) | Memory: 45.6 MB (76.40%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -15,24 +15,26 @@
  */
 class Solution {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-      return fn(preorder,inorder,0,preorder.length-1,0,inorder.length-1);  
+     return   fn(inorder,preorder,0,inorder.length-1,0,preorder.length-1);
     }
-    public TreeNode fn(int[]pre,int[]in,int plo,int phi,int ilo,int ihi){
-      if(ilo>ihi || plo>phi)
-      return null;
-      TreeNode nn=new TreeNode(pre[plo]);
-      int idx=search(in,pre[plo]);
-      int c=idx-ilo;
-      nn.left=fn(pre,in,plo+1,plo+c,ilo,idx-1);
-      nn.right=fn(pre,in,plo+c+1,phi,idx+1,ihi);
-      return nn;
-    }
-public int search(int[]in,int item){
-    for(int i=0;i<in.length;i++){
-        if(in[i]==item)
-        return i;
-    }
+    public TreeNode fn(int[]in,int[]pre,int ilo,int ihi,int plo,int phi){
+        if(ilo>ihi || plo>phi)
+        return null;
+        TreeNode nn=new TreeNode();
+        nn.val=pre[plo];
+        int idx=search(in,ilo,ihi,pre[plo]);
+        int c=idx-ilo;
+        nn.left=fn(in,pre,ilo,idx-1,plo+1,plo+c);
+        nn.right=fn(in,pre,idx+1,ihi,plo+c+1,phi);
+        return nn;
 
-    return -1;
-}
+
+    }
+    public int search(int[]in,int ilo,int ihi,int item){
+	        for(int i=ilo;i<=ihi;i++){
+	            if(in[i]==item)
+	            return i;
+	        }
+	        return 0;
+	    }
 }
