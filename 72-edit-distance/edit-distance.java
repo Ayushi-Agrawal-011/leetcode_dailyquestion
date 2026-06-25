367b52f7d6dee49aaea47604785562810903e53e
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2026-06-25 18:45:41 +0530

    Time: 3 ms (98.36%) | Memory: 47.5 MB (6.25%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -1,31 +1,28 @@
 class Solution {
-    public int minDistance(String s, String t) {
-    int n=s.length();
-    int m=t.length();
-    int[][]dp=new int[n][m];
-    for(int[]row:dp){
-        Arrays.fill(row,-1);
+    public int minDistance(String word1, String word2) {
+        int[][]dp=new int[word1.length()][word2.length()];
+        for(int[]ar:dp){
+            Arrays.fill(ar,-1);
+        }
+        return fn(word1,word2,0,0,dp);
     }
-return r(s,t,0,0,dp);
-    }
-public int r(String s,String t,int i,int j,int[][]dp){
-    if(i==s.length()){
-        return t.length()-j;
-    }
-    if(j==t.length()){
+    public int fn(String s,String t,int i,int j,int[][]dp){
+         if(j==t.length())
         return s.length()-i;
+        if(i==s.length())
+        return t.length()-j;
+       
+        if(dp[i][j]!=-1)
+        return dp[i][j];
+        if(s.charAt(i)==t.charAt(j)){
+            return dp[i][j]=fn(s,t,i+1,j+1,dp);
+        }
+        else{
+            int a=fn(s,t,i,j+1,dp);
+            int b=fn(s,t,i+1,j,dp);
+            int c=fn(s,t,i+1,j+1,dp);
+            return dp[i][j]=1+Math.min(a,Math.min(b,c));
+        }
+        //return dp[i][j];
     }
-    if(dp[i][j]!=-1)
-    return dp[i][j];
-    if(s.charAt(i)==t.charAt(j)){
-        return dp[i][j]=r(s,t,i+1,j+1,dp);
-    }
-    else{
-        int D=r(s,t,i,j+1,dp);
-        int I =r(s,t,i+1,j,dp);
-         int  R=r(s,t,i+1,j+1,dp);
-         return dp[i][j]=Math.min(D,Math.min(I,R))+1;
-    }
-  
-}
 }
