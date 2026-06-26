3a321d8d9532134143c3a684826d07bd48c62dfa
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2026-06-26 18:25:54 +0530

    Time: 234 ms (8.37%) | Memory: 55.2 MB (16.52%) - LeetSync

--- 1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.java
+++ 1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.java
@@ -1,36 +1,27 @@
 class Solution {
-    public int minFallingPathSum(int[][] grid) {
-   int ans=Integer.MAX_VALUE;
-  int n=grid.length;
-  int[][]dp=new int[n][n];
-  for(int[]ar:dp){
-    Arrays.fill(ar,-66666);
-  }
-  for(int i=0;i<grid[0].length;i++){
-    ans=Math.min(ans,fn(grid,0,i,n,n,dp));
-  }
-return ans;
+    public int minFallingPathSum(int[][] grid) {  
+    int[][]dp=new int[grid.length][grid[0].length];
+        for(int[]ar:dp)
+        Arrays.fill(ar,-66666);
+        int ans=Integer.MAX_VALUE;
+        for(int j=0;j<grid[0].length;j++){
+            ans=Math.min(ans,fn(grid,0,j,dp));
+        }
+        return ans;
     }
-
- public int fn(int[][]grid,int i,int j,int m,int n,int [][] dp){
-        if(i<0 || j<0 || i>=m || j>=n){
+    public int fn(int[][]grid,int i,int j,int[][]dp){
+            if (i >= grid.length || j >= grid[0].length || i<0 || j<0)
             return Integer.MAX_VALUE;
-        }
-        if(i==m-1){
+        if (i == grid.length - 1 )
             return grid[i][j];
-        }
         if(dp[i][j]!=-66666)
         return dp[i][j];
         int ans=Integer.MAX_VALUE;
-        for(int k=0;k<n;k++){
+        for(int k=0;k<grid[0].length;k++){
             if(k==j)
-            continue;
-             int ld=fn(grid,i+1,k,m,n,dp);
-   ans=Math.min(ans,ld);
-         
-       
+continue;
+    ans=Math.min(ans,fn(grid,i+1,k,dp));
         }
-
-return dp[i][j]=ans+grid[i][j];
+        return dp[i][j]=ans+grid[i][j];
     }
 }
