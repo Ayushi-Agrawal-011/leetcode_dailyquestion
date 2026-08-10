4ab7caeedb281ce297987e1e63b65d0a37c5706b
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2026-08-10 09:42:41 +0530

    Time: 2779 ms (5.05%) | Memory: 48.3 MB (93.97%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.java
@@ -1,30 +1,28 @@
 class Solution {
-    public int subarraySum(int[]arr, int k) {
-    int ans=0,sum=0;
-    int r=k;
-for(int  i=1;i<=arr.length;i++){
-int l=fn(arr,i, r);
-ans+=l;
-//System.out.println(l);
-}
-return ans;
+    public int subarraySum(int[] arr, int k) {
+        int ans=0;
+  for(int i=1;i<=arr.length;i++){
+ans+=fn(arr,i,k);
+  }
+  return ans;
+
     }
-    public int fn(int[]arr,int k,int r){
-        int ans=0,sum=0;
-    for(int i=0;i<k;i++){
-        sum+=arr[i];
-    } 
-    ans=sum;
-    int c=0;
-    if(sum==r)
-    c=1;
-    for(int i=k;i<arr.length;i++){
-        sum+=(arr[i]-arr[i-k]);
-        //System.out.println(sum);
-        if(sum==r)
+    public int fn(int[]arr,int k,int t){
+int ans=0,sum=0;
+for(int i=0;i<k;i++){
+sum+=arr[i];
+}
+int c=0;
+if(sum==t){
+    c++;
+}
+for(int i=k;i<arr.length;i++){
+    sum+=arr[i]-arr[i-k];
+    if(sum==t){
         c++;
     }
-    return c;
+}
+return c;
     }
 
 }
