80950c8a1407795b657666448ac6fc4604e96952
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2026-08-15 18:34:13 +0530

    Time: 9 ms (25.56%) | Memory: 65.2 MB (58.61%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.java
+++ 131-palindrome-partitioning/palindrome-partitioning.java
@@ -1,34 +1,35 @@
-public class Solution {
+class Solution {
     public List<List<String>> partition(String s) {
-    List<List<String>> res=new ArrayList<>();
-    List<String> cur=new ArrayList<>();
-    part(s,res,cur);
-    return res;
+      List<List<String>> ans=new ArrayList<>();
+ fn(s,new ArrayList<>(),ans);  
+ return ans;
     }
-public void part(String s,    List<List<String>> res,List<String> cur){
-   if(s.length()==0){
-    res.add(new ArrayList<>(cur));
-    return;
-   }
-   for(int i=1;i<=s.length();i++){
-    String p=s.substring(0,i);
-    if(ispal(p)){
-        cur.add(p);
-        part(s.substring(i),res,cur);
-        cur.remove(cur.size()-1);
+    public void fn (String s,List<String> ll,List<List<String>> p){
+        if(s.length()==0){
+  
+            p.add(new ArrayList<String>(ll));
+            return;
+        }
+        for(int cut=1;cut<=s.length();cut++){
+            String t=s.substring(0,cut);
+            if(ispal(t)){
+                ll.add(t);
+                fn(s.substring(cut),ll,p);
+                ll.remove(ll.size()-1);
+            }
+        }
     }
-   }
-}
-public boolean ispal(String s){
-    int i=0,j=s.length()-1;
-    while(i<j){
-        if(s.charAt(i)!=s.charAt(j))
-        return false;
-        i++;
-        j--;
-    }
-    return true;
-}
-
+    public static boolean ispal(String s) {
+		int i = 0;
+		int j = s.length() - 1;
+		while (i < j) {
+			if (s.charAt(i) != s.charAt(j)) {
+				return false;
+			}
+			i++;
+			j--;
+		}
+		return true;
+	}
 
 }
