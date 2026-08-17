cea3311bbad3b4bfa0a3fde77fb6ac2b5940d1af
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2026-08-17 11:38:04 +0530

    Time: 32 ms (9.56%) | Memory: 53.8 MB (82.11%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,25 +1,26 @@
 class Solution {
-    public int longestCommonSubsequence(String text1, String text2) {
-          int[][]dp=new int[text1.length()][text2.length()];
-        for(int[]ar:dp){
-            Arrays.fill(ar,-1);
+    public int longestCommonSubsequence(String s, String t) {
+        int n = s.length();
+        int m = t.length();
+        int[][] dp = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(dp[i], -1);
         }
-return fn(text1,text2,0,0,dp);
+        return fn(s,t,0,0,dp);
     }
-     public int fn(String s,String t,int i,int j,int[][]dp){
-        if( i>=s.length() || j>=t.length())
+    public int fn(String s,String t,int i,int j,int[][]dp){
+        if(i>=s.length() || j>=t.length())
         return 0;
-      
         if(dp[i][j]!=-1)
         return dp[i][j];
         if(s.charAt(i)==t.charAt(j)){
-return  dp[i][j]=1+fn(s,t,i+1,j+1,dp);
+            return dp[i][j]=1+fn(s,t,i+1,j+1,dp);
         }
         else{
-        int a=fn(s,t,i,j+1,dp);
-        int b=fn(s,t,i+1,j,dp);
-       
-        return dp[i][j]=Math.max(a,b);
+            int a=fn(s,t,i+1,j,dp);
+            int b=fn(s,t,i,j+1,dp);
+            return dp[i][j]=Math.max(a,b);
+        
         }
     }
 }
