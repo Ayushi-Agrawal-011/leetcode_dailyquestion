7cb261ce03b969c3c1c30ef97261c54a88c16ce0
Ayushi-Agrawal-011 <ayushiagrawal0502@gmail.com>
2026-09-12 14:17:23 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (5.78%) - LeetSync

--- 809-preimage-size-of-factorial-zeroes-function/preimage-size-of-factorial-zeroes-function.java
+++ 809-preimage-size-of-factorial-zeroes-function/preimage-size-of-factorial-zeroes-function.java
@@ -1,34 +1,38 @@
-class Solution {
-    public int preimageSizeFZF(int k) {
-        if(k<5)
-        return 5;
-        if(k==1000000000)
-        return 5;
-     return   fn(k);
-    }
-    public int fn(int k){
-        int low=0;
-        int high=5*k;
-        int ans=0;
-        while(low<=high){
-       int mid=low+(high-low)/2;
-       int p=count(mid);
-       if(p<k)
-          low=mid+1;
-    
-       else if(p>k)
-         high=mid-1;
-       else
-return 5;
-        }
-        return ans;
-    }
-    public int count (int n){
-        int ans=0;
-        while(n>0){
-            n=n/5;
-            ans+=n;
-        }
-        return ans;
-    }
+class Solution { 
+    public int preimageSizeFZF(int k) { 
+        if(k < 5) 
+            return 5; 
+        
+        return fn(k); 
+    } 
+
+    public int fn(int k){ 
+        long low=0; 
+        long high=5L*k; 
+        
+        while(low<=high){ 
+            long mid=low+(high-low)/2; 
+            long p=count(mid); 
+            
+            if(p<k) 
+                low=mid+1; 
+            else if(p>k) 
+                high=mid-1; 
+            else 
+                return 5; 
+        } 
+        
+        return 0; 
+    } 
+
+    public long count(long n){ 
+        long ans=0; 
+        
+        while(n>0){ 
+            n=n/5; 
+            ans+=n; 
+        } 
+        
+        return ans; 
+    } 
 }
